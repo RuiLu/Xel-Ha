f70c4dce0a82fa0343802979a480ebf4314e4069
RuiLu <rlu0213@hotmail.com>
2016-08-17 17:22:51 -0400

    231

--- 231_PowerOfTwo.java
+++ 231_PowerOfTwo.java
@@ -1,11 +1,17 @@
 public class Solution {
+    /**
+     * First method -> Iteration
+     */ 
+    // public boolean isPowerOfTwo(int n) {
+    //     if (n == 0) return false;
+    //     while (n % 2 == 0) n /= 2;
+    //     return n == 1;
+    // }
+    
+    /**
+     *  Second method -> Bit manipulation
+     */ 
     public boolean isPowerOfTwo(int n) {
-        // 1. iterative
-        if (n == 0) return false;
-        while (n % 2 == 0) n /= 2;
-        return n == 1;
-        
-        // 2. bit manipulation
-        return n > 0 && ((n & (n - 1)) == 0);
+        return (n > 0 && (n & (n-1)) == 0);
     }
 }
