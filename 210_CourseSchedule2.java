c2cd07f420e605e6620ba9038b18ab4b285a6bba
RuiLu <rlu0213@hotmail.com>
2016-08-22 23:51:46 -0400

    210

--- 210_CourseSchedule2.java
+++ 210_CourseSchedule2.java
@@ -9,44 +9,48 @@
     _ -- [3,1,2,0]
 */
 public class Solution {
+    
+    /**
+     *  1. DFS 
+     */
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> list = new ArrayList<>();
-        for (int i = 0; i < numCourses; ++i) {
-            list.add(new ArrayList<>());
+        
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) lists.add(new ArrayList<>());
+        
+        for (int[] prerequisite : prerequisites) {
+            lists.get(prerequisite[1]).add(prerequisite[0]);
         }
-        for (int[] edge : prerequisites) {
-            list.get(edge[1]).add(edge[0]);
-        }
-        int[] status = new int[numCourses]; // 0->unvisited; 1->visiting; 2->visited
-        LinkedList<Integer> topoSeq = new LinkedList<>();
-        for (int i = 0; i < numCourses; ++i) {
-            if (status[i] == 0) {
-                if (existCircle(topoSeq, list, status, i)) {
-                    return new int[0];
-                }
+        
+        int[] visited = new int[numCourses];
+        LinkedList<Integer> sequence = new LinkedList<>();
+        
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0) {
+              if (hasCycle(sequence, visited, lists, i)) return new int[0]; 
             }
         }
-        int[] res = new int[topoSeq.size()];
-        for (int i = 0; i < res.length; ++i) {
-            res[i] = topoSeq.get(i);
-        }
+        
+        int[] res = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) res[i] = sequence.get(i);
+        
         return res;
     }
     
-    public boolean existCircle(LinkedList<Integer> topoSeq, List<List<Integer>> list, int[] status, int index) {
-        status[index] = 1;
-        for (int next : list.get(index)) {
-            if (status[next] == 1) {
-                return true;
-            }
-            if (status[next] == 0) {
-                if (existCircle(topoSeq, list, status, next)) {
-                    return true;
-                }
+    private boolean hasCycle(LinkedList<Integer> seq, int[] visited, List<List<Integer>> lists, int index) {
+        if (visited[index] == 1) return true;
+        
+        visited[index] = 1;
+        for (int next : lists.get(index)) {
+            if (visited[next] == 1) return true;    // exist a cycle
+            if (visited[next] == 0) {
+                if (hasCycle(seq, visited, lists, next)) return true;
             }
         }
-        topoSeq.addFirst(index);
-        status[index] = 2;
+        
+        visited[index] = 2;
+        seq.addFirst(index);
+        
         return false;
     }
 }
