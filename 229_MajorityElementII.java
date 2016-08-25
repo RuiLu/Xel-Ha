9277391f2979399c0ca68278df1f19fa48655411
RuiLu <rlu0213@hotmail.com>
2016-08-25 13:51:57 -0400

    229

---
+++ 229_MajorityElementII.java
@@ -0,0 +1,39 @@
+public class Solution {
+    /**
+     *  There will be at most two elements in the result
+     */
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        
+        int num1 = 0, num2 = 0, count1 = 0, count2 = 0;
+        
+        for (int num : nums) {
+            if (num == num1) count1++;
+            else if (num == num2) count2++;
+            else if (count1 == 0) {
+                num1 = num;
+                count1++;
+            } else if (count2 == 0) {
+                num2 = num;
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        
+        for (int num : nums) {
+            if (num == num1) count1++;
+            else if (num == num2) count2++;
+        }
+        
+        if (count1 > nums.length / 3) res.add(num1);
+        if (count2 > nums.length / 3) res.add(num2);
+        
+        return res;
+    }
+}
