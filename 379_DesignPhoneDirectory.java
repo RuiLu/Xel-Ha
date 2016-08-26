aebc274b59a077a7a8637f0441388f802ffdb59d
RuiLu <rlu0213@hotmail.com>
2016-08-26 17:19:06 -0400

    379

--- 379_DesignPhoneDirectory.java
+++ 379_DesignPhoneDirectory.java
@@ -43,6 +43,35 @@ public class PhoneDirectory {
 }
 
 /**
+ *  2. Extend BitSet. Such a brilliant idea!!
+ *  Reference -> https://discuss.leetcode.com/topic/53120/subclassing-set-python-java
+ *  API reference -> https://docs.oracle.com/javase/7/docs/api/java/util/BitSet.html
+ */ 
+public class PhoneDirectory extends BitSet {
+    
+    public PhoneDirectory(int maxNumbers) {
+        super(maxNumbers);
+        flip(0, maxNumbers);
+    }
+    
+    public int get() {
+        if (isEmpty()) return -1;
+        
+        int number = nextSetBit(0);
+        clear(number);
+        return number;
+    }
+    
+    public boolean check(int number) {
+        return get(number);
+    }
+    
+    public void release(int number) {
+        set(number);
+    }
+}
+
+/**
  * Your PhoneDirectory object will be instantiated and called as such:
  * PhoneDirectory obj = new PhoneDirectory(maxNumbers);
  * int param_1 = obj.get();
