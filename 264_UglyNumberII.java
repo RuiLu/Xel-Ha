efc188d12ceb3a11378f3184d21467ac19846e64
RuiLu <rlu0213@hotmail.com>
2016-08-31 23:44:37 -0400

    264

--- 264_UglyNumberII.java
+++ 264_UglyNumberII.java
@@ -3,6 +3,37 @@
  *  Ugly number can be only obtained by various kinds of arithmetic operations between 2, 3, and 5.
  */
 public class Solution {
+    /**
+     *  Time complexity -> O(3n) = O(n)
+     *  Space complexity -> O(n)
+     */
+    public int nthUglyNumber(int n) {
+        if (n == 1) return 1;
+        
+        int[] ugly = new int[n];
+        int[] idx = new int[3];
+        int[] val = {1, 1, 1};
+        int[] primes = {2, 3, 5};
+        
+        int next = 1;
+        
+        for (int i = 0; i < n; i++) {
+            ugly[i] = next;
+            
+            next = Integer.MAX_VALUE;
+            for (int j = 0; j < 3; j++) {
+                if (val[j] == ugly[i]) val[j] = ugly[idx[j]++] * primes[j];
+                next = Math.min(next, val[j]);
+            }
+        }
+        
+        return ugly[n - 1];
+    }
+    
+    /**
+     *  Time complexity -> O(n * logk), k is the size of priority queue
+     *  Space complexity -> O(n + ...)
+     */
     public int nthUglyNumber(int n) {
         if (n == 1) return 1;
         
