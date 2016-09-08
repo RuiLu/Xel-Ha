27f015cea8fdf607fc39f071afc5b65ae1aac09c
RuiLu <rlu0213@hotmail.com>
2016-09-07 21:48:48 -0400

    394

--- 394_DecodeString.java
+++ 394_DecodeString.java
@@ -1,4 +1,4 @@
-c class Solution {
+public class Solution {
     /**
      *  Recursion
      */
@@ -27,6 +27,44 @@ c class Solution {
                 String duplicates = decodeString(s.substring(par + 1, i));
                 while (num-- > 0) res += duplicates;
                 
+            }
+        }
+        
+        return res;
+    }
+
+    /**
+     *  Stack
+     */
+    public String decodeString(String s) {
+        String res = "";
+        if (s == null || s.length() == 0) return res;
+        
+        Stack<String> prevString = new Stack<>();
+        Stack<Integer> count = new Stack<>();
+        
+        count.push(1);
+        
+        for (int i = 0; i < s.length();) {
+            char curr = s.charAt(i);
+            if (Character.isDigit(curr)) {
+                int start = i;
+                while (Character.isDigit(s.charAt(++i)));
+                int repeatTimes = Integer.valueOf(s.substring(start, i));
+                count.push(repeatTimes);
+            } else if (curr == '[') {
+                prevString.push(res);
+                res = "";
+                i++;
+            } else if (curr == ']') {
+                StringBuilder sb = new StringBuilder(prevString.pop());
+                int repeatTimes = count.pop();
+                while (repeatTimes-- > 0) sb.append(res);
+                res = sb.toString();
+                i++;
+            } else {
+                res += curr;
+                i++;
             }
         }
         
