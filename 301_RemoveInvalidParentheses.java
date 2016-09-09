fb8cacfcf89b410e8a585e10ce2ffc22e8b56fa7
RuiLu <rlu0213@hotmail.com>
2016-09-08 20:52:57 -0400

    301

--- 301_RemoveInvalidParentheses.java
+++ 301_RemoveInvalidParentheses.java
@@ -5,29 +5,27 @@ public class Solution {
      */ 
     public List<String> removeInvalidParentheses(String s) {
         List<String> res = new ArrayList<>();
-        remove(s, res, 0, 0, new char[]{'(', ')'});
+        remove(s, res, 0, new char[]{'(', ')'});
         return res;
     }
     
-    private void remove(String s, List<String> res, int prevI, int prevJ, char[] par) {
-        for (int stack = 0, i = 0; i < s.length(); i++) {
+    private void remove(String s, List<String> res, int prevJ, char[] par) {
+        int stack = 0;
+        for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == par[0]) stack++;
             if (s.charAt(i) == par[1]) stack--;
             if (stack >= 0) continue;
+            
             for (int j = prevJ; j <= i; j++) {
                 if (s.charAt(j) == par[1] && (j == prevJ || s.charAt(j - 1) != par[1])) {
-                    // The reason why we directly pass j here is that we have already reduced the length of s by one
-                    remove(s.substring(0, j) + s.substring(j + 1), res, i, j, par);
+                    remove(s.substring(0, j) + s.substring(j + 1), res, j, par);
                 }
             }
             return;
         }
         
         String reversed = new StringBuilder(s).reverse().toString();
-        if (par[0] == '(') {
-            remove(reversed, res, 0, 0, new char[]{')', '('});
-        } else {
-            res.add(reversed);
-        }
+        if (par[0] == '(') remove(reversed, res, 0, new char[]{')', '('});
+        else res.add(reversed);
     }
 }
