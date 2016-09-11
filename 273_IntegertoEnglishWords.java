aeb2338834615db1138747ba7084b6d07f04e235
RuiLu <rlu0213@hotmail.com>
2016-09-11 10:45:48 -0400

    273

--- 273_IntegertoEnglishWords.java
+++ 273_IntegertoEnglishWords.java
@@ -1,4 +1,33 @@
 public class Solution {
+    private final String[] BELOWTWENTY = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+    
+    public String numberToWords(int num) {
+        if (num == 0)  return "Zero";
+        
+        int i = 0;
+        String res = "";
+        
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                res = helper(num % 1000) + THOUSANDS[i] + " " + res; 
+            }
+            num /= 1000;
+            i++;
+        }
+        
+        return res.trim();
+    }
+    
+    private String helper(int num) {
+        if (num == 0) return "";
+        else if (num < 20) return BELOWTWENTY[num] + " ";
+        else if (num < 100) return TENS[num / 10] + " " + helper(num % 10);
+        else return BELOWTWENTY[num / 100] + " Hundred " + helper(num % 100);
+    }
+
+
     /**
      *  The following final parameters are referred from ->
      *  https://discuss.leetcode.com/topic/30488/short-clean-java-solution
