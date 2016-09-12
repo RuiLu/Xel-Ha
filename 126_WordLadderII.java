d14be14045f22cf3158ec8495d3a8bdc563c41d4
RuiLu <rlu0213@hotmail.com>
2016-09-12 11:07:27 -0400

    126

--- 126_WordLadderII.java
+++ 126_WordLadderII.java
@@ -3,26 +3,27 @@ public class Solution {
      *  Reference -> https://discuss.leetcode.com/topic/2857/share-two-similar-java-solution-that-accpted-by-oj/2
      *  In general, use BFS to construct a graph, then use DFS to construct path from end to start
      */
-    List<List<String>> results;
     Map<String, List<String>> map;
+    List<List<String>> res;
     
-    public List<List<String>> findLadders(String beginWord, String endWord, Set<String> wordList) {
-        results = new ArrayList<>();
-        if (wordList.size() == 0) return results;
+    public List<List<String>> findLadders(String startWord, String endWord, Set<String> wordList) {
+        res = new ArrayList<>();
+        if (wordList.size() == 0) return res;
         
         map = new HashMap<>();
         
-        int min = Integer.MAX_VALUE;
-        
         Queue<String> queue = new LinkedList<>();
-        queue.offer(beginWord);
+        queue.offer(startWord);
         
         Map<String, Integer> ladder = new HashMap<>();
         for (String word : wordList) ladder.put(word, Integer.MAX_VALUE);
-        ladder.put(beginWord, 0);
+        ladder.put(startWord, 0);
+        
+        int min = Integer.MAX_VALUE;
         
         wordList.add(endWord);
         
+        /* BFS -> Dijisktra search */
         while (!queue.isEmpty()) {
             String word = queue.poll();
             
@@ -31,24 +32,19 @@ public class Solution {
             
             for (int i = 0; i < word.length(); i++) {
                 StringBuilder sb = new StringBuilder(word);
-                for (char c = 'a'; c <= 'z'; c++) {
-                    sb.setCharAt(i, c);
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    sb.setCharAt(i, ch);
                     String next = sb.toString();
                     
                     if (ladder.containsKey(next)) {
-                        if (step > ladder.get(next)) continue;  // check if it is the shortest path
-                        else if (step < ladder.get(next)) {
+                        if (ladder.get(next) < step) continue;
+                        else if (ladder.get(next) > step) {
                             ladder.put(next, step);
                             queue.offer(next);
                         }
                         
-                        if (map.containsKey(next)) {
-                            map.get(next).add(word);
-                        } else {
-                            List<String> list = new ArrayList<>();
-                            list.add(word);
-                            map.put(next, list);
-                        }
+                        if (!map.containsKey(next)) map.put(next, new ArrayList<>());
+                        map.get(next).add(word);
                         
                         if (next.equals(endWord)) min = step;
                     }
@@ -56,24 +52,25 @@ public class Solution {
             }
         }
         
-        List<String> res = new LinkedList<>();
-        backtracking(endWord, beginWord, res);
+        /* Backtracking */
+        List<String> tmp = new LinkedList<>();
+        backtracking(endWord, startWord, tmp);
         
-        return results;
+        return res;
     }
     
-    private void backtracking(String word, String start, List<String> list) {
+    private void backtracking(String word, String start, List<String> tmp) {
         if (word.equals(start)) {
-            list.add(0, start);
-            results.add(new ArrayList<>(list));
-            list.remove(0); // backtracking
+            tmp.add(0, start);
+            res.add(new ArrayList<>(tmp));
+            tmp.remove(0);
             return;
         }
         
-        list.add(0, word);
+        tmp.add(0, word);
         if (map.get(word) != null) {
-            for (String next : map.get(word)) backtracking(next, start, list);
+            for (String next : map.get(word)) backtracking(next, start, tmp);
         }
-        list.remove(0);     // backtracking
+        tmp.remove(0);
     }
 }
