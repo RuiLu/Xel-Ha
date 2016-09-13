76518216de7c6197a1b646bc0471f79a94f1800e
RuiLu <rlu0213@hotmail.com>
2016-09-12 23:48:35 -0400

    275

--- 275_HIndexII.java
+++ 275_HIndexII.java
@@ -19,4 +19,20 @@ public class Solution {
         
         return len - (hi + 1);
     }
+
+    public int hIndex(int[] citations) {
+        if (citations == null || citations.length == 0) return 0;
+        
+        int len = citations.length;
+        int lo = 0;
+        int hi = len - 1;
+        
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+            if ((len - mid) <= citations[mid]) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        
+        return len - lo;
+    }
 }
