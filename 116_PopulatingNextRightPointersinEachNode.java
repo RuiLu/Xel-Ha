a28c069d371eef6d1a534ae66b1a5fd4e6dd1e77
RuiLu <rlu0213@hotmail.com>
2016-09-12 20:37:44 -0400

    116

--- 116_PopulatingNextRightPointersinEachNode.java
+++ 116_PopulatingNextRightPointersinEachNode.java
@@ -7,9 +7,48 @@
  * }
  */
 public class Solution {
-    // 1. DFS
+    /**
+     *  First -> BFS
+     */
     public void connect(TreeLinkNode root) {
         if (root == null) return;
+        
+        int currNum = 1;
+        int nextNum = 0;
+        Queue<TreeLinkNode> queue = new LinkedList<>();
+        
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            TreeLinkNode curr = queue.poll();
+            
+            currNum--;
+            
+            if (curr.left != null) {
+                queue.offer(curr.left);
+                nextNum++;
+            }
+            if (curr.right != null) {
+                queue.offer(curr.right);
+                nextNum++;
+            }
+            
+            if (currNum != 0) {
+                curr.next = queue.peek();
+            } else {
+                curr.next = null;
+                currNum = nextNum;
+                nextNum = 0;
+            }
+        }
+    }
+    
+    /**
+     *  Second -> Iteration
+     */
+    public void connect(TreeLinkNode root) {
+        if (root == null) return;
+        
         while (root.left != null) {
             TreeLinkNode temp = root;
             while (temp != null) {
@@ -20,32 +59,5 @@ public class Solution {
             root = root.left;
         }
     }
-    
-    // 2. BFS
-    public void connect(TreeLinkNode root) {
-        if (root == null) return;
-        LinkedList<TreeLinkNode> queue = new LinkedList<>();
-        queue.add(root);
-        int currNum = 1;
-        int nextNum = 0;
-        while (!queue.isEmpty()) {
-            TreeLinkNode node = queue.poll();
-            currNum--;
-            if (node.left != null) {
-                queue.add(node.left);
-                nextNum++;
-            }
-            if (node.right != null) {
-                queue.add(node.right);
-                nextNum++;
-            }
-            if (currNum == 0) {
-                node.next = null;
-                currNum = nextNum;
-                nextNum = 0;
-            } else {
-                node.next = queue.peek();
-            }
-        }
-    }
-}
+
+} 
