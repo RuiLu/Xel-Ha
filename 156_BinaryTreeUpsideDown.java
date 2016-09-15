2667b008d01f27d259ed3a16c6463a4dfcf2a3ff
RuiLu <rlu0213@hotmail.com>
2016-09-15 18:21:57 -0400

    156

--- 156_BinaryTreeUpsideDown.java
+++ 156_BinaryTreeUpsideDown.java
@@ -31,6 +31,21 @@ public class Solution {
         
         return prev;
     }
-    
+
+    /**
+     *  Recursion
+     */
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) return root;
+        
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        
+        return newRoot;
+    }    
     
 }
