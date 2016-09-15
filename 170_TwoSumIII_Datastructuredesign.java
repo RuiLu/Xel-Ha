6a9d0269fdcaf7c460470db418af626f29046542
RuiLu <rlu0213@hotmail.com>
2016-09-15 16:21:18 -0400

    170

--- 170_TwoSumIII_Datastructuredesign.java
+++ 170_TwoSumIII_Datastructuredesign.java
@@ -3,32 +3,32 @@ public class TwoSum {
      *  First, like normal Two Sum solution
      *  Take too much time
      */
-//     private List<Integer> nums = new ArrayList<>();
+    private List<Integer> nums = new ArrayList<>();
     
-//     // Add the number to an internal data structure.
-// 	public void add(int number) {
-// 	    nums.add(number);
-// 	}
+    // Add the number to an internal data structure.
+	public void add(int number) {
+	    nums.add(number);
+	}
 
-//     // Find if there exists any pair of numbers which sum is equal to the value.
-// 	public boolean find(int value) {
-// 	    if (nums.size() <= 1) return false;
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean find(int value) {
+	    if (nums.size() <= 1) return false;
 	    
-// 	    Map<Integer, Integer> map = new HashMap<>();
+	    Map<Integer, Integer> map = new HashMap<>();
 	    
-// 	    for (int num : nums) {
-// 	        if (map.containsKey(num)) return true;
-// 	        map.put(value - num, num);
-// 	    }
+	    for (int num : nums) {
+	        if (map.containsKey(num)) return true;
+	        map.put(value - num, num);
+	    }
 	    
-// 	    return false;
-// 	}
+	    return false;
+	}
 	
 	/**
-	 *  Second, using two set, once adding a new number, update the num set and sum set
+	 *  Second, using a list, and a hashMap
 	 */
-	private List<Integer> nums = new ArrayList<>();
 	private Map<Integer, Integer> map = new HashMap<>();
+	private List<Integer> nums = new ArrayList<>();
 	
 	public void add(int number) {
 	    if (map.containsKey(number)) map.put(number, map.get(number) + 1);
@@ -39,10 +39,10 @@ public class TwoSum {
 	}
 	
 	public boolean find(int value) {
-        for (int i = 0; i < nums.size(); i++) {
-            int a = nums.get(i);
-            int b = value - a;
-            if ((a == b && map.get(a) > 1) || (a != b && map.containsKey(b))) return true;
+	    for (int num : nums) {
+	        int a = num;
+	        int b = value - a;
+	        if ((a == b && map.get(a) > 1) || (a != b && map.containsKey(b))) return true;
 	    }
 	    
 	    return false;
