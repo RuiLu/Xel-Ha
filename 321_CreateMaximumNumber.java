e0a478fd9048d8b62aea6cadb1653b4e8e9bdafb
RuiLu <rlu0213@hotmail.com>
2016-09-16 10:05:48 -0400

    321

--- 321_CreateMaximumNumber.java
+++ 321_CreateMaximumNumber.java
@@ -7,45 +7,42 @@ public class Solution {
      *  2. Figure out which one is the largest one
      */
     public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int[] res = new int[k];
         int len1 = nums1.length;
         int len2 = nums2.length;
-        int[] res = new int[k];
         
         for (int i = Math.max(0, k - len2); i <= k && i <= len1; i++) {
-            int[] maxSubArray1 = getMaxArray(nums1, i);
-            int[] maxSubArray2 = getMaxArray(nums2, k - i);
-            int[] candidate = merge(maxSubArray1, maxSubArray2, k);
+            int[] subMaxArray1 = getSubMaxArray(nums1, i);
+            int[] subMaxArray2 = getSubMaxArray(nums2, k - i);
+            int[] candidate = merge(subMaxArray1, subMaxArray2, k);
             if (greater(candidate, 0, res, 0)) res = candidate;
         }
         
         return res;
     }
     
-    private boolean greater(int[] nums1, int i, int[] nums2, int j) {
-        while (i < nums1.length && j < nums2.length && nums1[i] == nums2[j]) {
-            i++;
-            j++;
+    private boolean greater(int[] nums1, int len1, int[] nums2, int len2) {
+        while (len1 < nums1.length && len2 < nums2.length && nums1[len1] == nums2[len2]) {
+            len1++;
+            len2++;
         }
         
-        /* Is equal or nums1 is greater than nums2 */
-        return j == nums2.length || (i < nums1.length && nums1[i] > nums2[j]);
+        return len2 == nums2.length || (len1 < nums1.length && nums1[len1] > nums2[len2]);
     }
     
     private int[] merge(int[] nums1, int[] nums2, int k) {
         int[] res = new int[k];
-        int idx1 = 0;
-        int idx2 = 0;
         
-        for (int r = 0; r < k; r++) {
-            res[r] = greater(nums1, idx1, nums2, idx2) ? nums1[idx1++] : nums2[idx2++];
+        for (int i = 0, j = 0, r = 0; r < k; r++) {
+            res[r] = greater(nums1, i, nums2, j) ? nums1[i++] : nums2[j++];
         }
         
         return res;
     }
     
-    private int[] getMaxArray(int[] nums, int k) {
-        int[] res = new int[k];
+    private int[] getSubMaxArray(int[] nums, int k) {
         int len = nums.length;
+        int[] res = new int[k];
         
         for (int i = 0, j = 0; i < len; i++) {
             while (len - i + j > k && j > 0 && res[j - 1] < nums[i]) j--;
