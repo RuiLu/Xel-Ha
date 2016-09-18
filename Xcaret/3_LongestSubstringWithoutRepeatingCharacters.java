77a69894f01c6f33fa55192cee0d0cb394b0bcaf
RuiLu <rlu0213@hotmail.com>
2016-09-18 17:21:10 -0400

    3

--- Xcaret/3_LongestSubstringWithoutRepeatingCharacters.java
+++ Xcaret/3_LongestSubstringWithoutRepeatingCharacters.java
@@ -5,18 +5,18 @@ public class Solution {
     public int lengthOfLongestSubstring(String s) {
         if (s == null || s.length() == 0) return 0;
         
-        Map<Character, Integer> map = new HashMap<>();
+        int j = -1;
+        int maxLen = Integer.MIN_VALUE;
         char[] sca = s.toCharArray();
-        int maxLen = 0;
-        int j = 0;
+        Map<Character, Integer> map = new HashMap<>();
         
         for (int i = 0; i < sca.length; i++) {
             if (map.containsKey(sca[i])) {
-                j = Math.max(j, map.get(sca[i]) + 1);
-            }
+                j = Math.max(j, map.get(sca[i]));
+            } 
             
+            maxLen = Math.max(maxLen, i - j);
             map.put(sca[i], i);
-            maxLen = Math.max(maxLen, i - j + 1);
         }
         
         return maxLen;
@@ -26,26 +26,26 @@ public class Solution {
      *  1. Brute Force
      *  Time complexity -> O(n^2)
      */
-    public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) return 0;
+    // public int lengthOfLongestSubstring(String s) {
+    //     if (s == null || s.length() == 0) return 0;
         
-        char[] sca = s.toCharArray();
-        Set<Character> set = new HashSet<>();
-        int maxLen = 1;
+    //     char[] sca = s.toCharArray();
+    //     Set<Character> set = new HashSet<>();
+    //     int maxLen = 1;
         
-        for (int i = 0; i < sca.length; i++) {
-            set.clear();
-            int end = i;
-            for (; end < sca.length; end++) {
-                if (set.contains(sca[end])) {
-                    maxLen = Math.max(maxLen, end - i);
-                    break;
-                }
-                set.add(sca[end]);
-            }
-            if (end == sca.length) maxLen = Math.max(maxLen, end - i);
-        }
+    //     for (int i = 0; i < sca.length; i++) {
+    //         set.clear();
+    //         int end = i;
+    //         for (; end < sca.length; end++) {
+    //             if (set.contains(sca[end])) {
+    //                 maxLen = Math.max(maxLen, end - i);
+    //                 break;
+    //             }
+    //             set.add(sca[end]);
+    //         }
+    //         if (end == sca.length) maxLen = Math.max(maxLen, end - i);
+    //     }
         
-        return maxLen;
-    }
+    //     return maxLen;
+    // }
 }
