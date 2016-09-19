f1f001597cc15424bb8a471a16833fb196ab2785
RuiLu <rlu0213@hotmail.com>
2016-09-19 10:55:51 -0400

    12

---
+++ Xcaret/12_IntegertoRoman.java
@@ -0,0 +1,13 @@
+public class Solution {
+    /**
+     *  Roman Wiki -> https://en.wikipedia.org/wiki/Roman_numerals
+     *  Solution Ref -> https://discuss.leetcode.com/topic/12384/simple-solution
+     */
+    public String intToRoman(int num) {
+        String[] M = {"", "M", "MM", "MMM"};
+        String[] C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+}
