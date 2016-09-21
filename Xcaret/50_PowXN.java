f49cb8a4c00a9f689c9dc567d87f01ebb1c9ba11
RuiLu <rlu0213@hotmail.com>
2016-09-21 17:20:29 -0400

    50

---
+++ Xcaret/50_PowXN.java
@@ -0,0 +1,19 @@
+public class Solution {
+    /**
+     *  Binary search
+     *  Time complexity -> O(logn)
+     */
+    public double myPow(double x, int n) {
+        double res = 1.0;
+        long absN = Math.abs((long)n);
+        
+        while (absN > 0) {
+            if ((absN & 1) == 1) res *= x;  // absN % 2 == 1
+            absN >>= 1;                     // absN / 2
+            x *= x;
+        }
+        
+        return n < 0 ? 1/res : res;
+    }
+    
+}
