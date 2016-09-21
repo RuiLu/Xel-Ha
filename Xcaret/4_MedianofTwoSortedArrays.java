3d497398cdfeb6882b8a39555dc3f8809481b514
RuiLu <rlu0213@hotmail.com>
2016-09-20 22:17:05 -0400

    4

--- Xcaret/4_MedianofTwoSortedArrays.java
+++ Xcaret/4_MedianofTwoSortedArrays.java
@@ -3,12 +3,14 @@ public class Solution {
      *  ************
      *  Time complexity -> O(log(min(m, n))), where m is length of nums1, n is length of nums2
      *  Reference -> https://discuss.leetcode.com/topic/16797/very-concise-o-log-min-m-n-iterative-solution-with-detailed-explanation/2
+     *  ************
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
         
-        if (len1 < len2) return findMedianSortedArrays(nums2, nums1);
+        /* ensure that nums1 is equal of longer than nums2 */
+        if (len1 < len2)  return findMedianSortedArrays(nums2, nums1);
         
         if (len2 == 0) return (nums1[(len1 - 1) / 2] + nums1[len1 / 2]) / 2.0;
         
@@ -19,6 +21,7 @@ public class Solution {
             int mid2 = (lo + hi) / 2;
             int mid1 = len1 + len2 - mid2;
             
+            /* Left part -> nums[(mid - 1) / 2], while right part -> nums[mid / 2] */
             double L1 = (mid1 == 0) ? Double.MIN_VALUE : nums1[(mid1 - 1) / 2];
             double L2 = (mid2 == 0) ? Double.MIN_VALUE : nums2[(mid2 - 1) / 2];
             double R1 = (mid1 == len1 * 2) ? Double.MAX_VALUE : nums1[mid1 / 2];
