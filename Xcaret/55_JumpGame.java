7d05e6e5e92a7d78e71b799ab011fb7cd38c5e77
RuiLu <rlu0213@hotmail.com>
2016-09-23 09:59:27 -0400

    55

--- Xcaret/55_JumpGame.java
+++ Xcaret/55_JumpGame.java
@@ -10,10 +10,10 @@ public class Solution {
         int foot = 0;
         
         for (int i = 0; i < nums.length; i++) {
-            max = Math.max(max, i + nums[i]);
-            
-            if (i == foot) foot = max;
-            
+            max = Integer.max(max, i + nums[i]);
+            if (i == foot) {
+                foot = max;
+            }
             if (foot >= nums.length - 1) return true;
         }
         
@@ -25,10 +25,13 @@ public class Solution {
      *  Time complexity -> O(n)
      */
     public boolean canJump(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
         int last = nums.length - 1;
+        
         for (int i = nums.length - 2; i >= 0; i--) {
             if (i + nums[i] >= last) last = i;
         }
+        
         return last <= 0;
     }
 }
