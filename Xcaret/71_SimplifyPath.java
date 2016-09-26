beda08664d15ac827db1d80079445a6226b61576
RuiLu <rlu0213@hotmail.com>
2016-09-26 15:06:22 -0400

    71

---
+++ Xcaret/71_SimplifyPath.java
@@ -0,0 +1,31 @@
+public class Solution {
+    /**
+     *  Using a stack-like array
+     *  Time complexity -> O(n)
+     *  Space complexity -> O(n)
+     */
+    public String simplifyPath(String path) {
+        if (path == null || path.length() == 0) return path;
+        
+        StringBuilder sb = new StringBuilder();
+        String[] tokens = path.split("/");
+        String[] stack = new String[tokens.length];
+        int count = 0;
+        
+        for (String token : tokens) {
+            if (token.equals(".") || token.equals("")) {
+                continue;
+            } else if (token.equals("..")) {
+                if (count > 0) count--;
+            } else {
+                stack[count++] = token;
+            }
+        }
+        
+        for (int i = 0; i < count; i++) {
+            sb.append("/" + stack[i]);
+        }
+        
+        return sb.length() == 0 ? "/" : sb.toString();
+    }
+}
