3477d6e2f854461a23a3e0662048684b971f3802
RuiLu <rlu0213@hotmail.com>
2016-09-26 22:02:21 -0400

    74

--- Xcaret/74_Searcha2DMatrix.java
+++ Xcaret/74_Searcha2DMatrix.java
@@ -5,30 +5,30 @@ public class Solution {
      *  so we can get Time Complexity -> O(logm + logn)
      */
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
         
         int row = matrix.length;
         int col = matrix[0].length;
-        
         int lo = 0;
         int hi = row - 1;
         
         while (lo < hi) {
             int mid = (lo + hi) / 2;
-            if (matrix[mid][col-1] == target) return true;
-            else if (matrix[mid][col-1] < target) lo = mid + 1;
-            else hi = mid;
+            if (matrix[mid][col-1] < target) lo = mid + 1;
+            else hi = mid;   
         }
         
-        int i = hi;
+        int rowIdx = hi;
         lo = 0;
         hi = col - 1;
         
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
-            if (matrix[i][mid] == target) return true;
-            else if (matrix[i][mid] < target) lo = mid + 1;
-            else hi = mid - 1;
+            if (matrix[rowIdx][mid] == target) return true;
+            else if (matrix[rowIdx][mid] > target) hi = mid - 1;
+            else lo = mid + 1;
         }
         
         return false;
