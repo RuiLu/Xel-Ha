be1b67e9d982b43791e1e4c8a2ce41df83eb5ce5
RuiLu <rlu0213@hotmail.com>
2016-09-27 15:13:41 -0400

    82

---
+++ Xcaret/82_RemoveDuplicatesfromSortedListII.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     *  Time complexity -> O(n)
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode fakeHead = new ListNode(-1);
+        fakeHead.next = head;
+        ListNode slow = fakeHead;
+        ListNode fast = head;
+        
+        while (fast != null) {
+            while (fast.next != null && fast.val == fast.next.val) {
+                fast = fast.next;
+            }
+            
+            if (slow.next == fast) {
+                slow = fast;
+                fast = fast.next;
+            } else {
+                slow.next = fast.next;
+                fast = slow.next;
+            }
+        }
+        
+        return fakeHead.next;
+    }
+}
