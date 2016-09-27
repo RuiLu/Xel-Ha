243956e21136cbfcbe2f98b01f65d723856a4046
RuiLu <rlu0213@hotmail.com>
2016-09-26 20:49:34 -0400

    44

--- Xcaret/44_WildcardMatching.java
+++ Xcaret/44_WildcardMatching.java
@@ -6,29 +6,27 @@ public class Solution {
     public boolean isMatch(String s, String p) {
         int sLen = s.length();
         int pLen = p.length();
-        
         boolean[][] dp = new boolean[sLen+1][pLen+1];
         dp[0][0] = true;
         
         for (int i = 0; i <= sLen; i++) {
             for (int j = 1; j <= pLen; j++) {
                 char curr = p.charAt(j-1);
-                
                 if (curr != '*') {
                     dp[i][j] = i > 0 && dp[i-1][j-1] && (curr == '?' || curr == s.charAt(i-1));
                 } else {
                     /**
-                     *  1. '*' is at the first position of pattern
-                     *  2. '*' is at the any position of pattern, except for the first position
+                     *  There are two situations: 
+                     *  1. '*' appears at the first position of p, so dp[i][j] is true;
+                     *  2. '*' appears at the other positions in p, so we need to check previous result to see
+                     *     if there is any matching 
                      */
-                    int k = i;
                     boolean isMatched = false;
-                    while (k >= 0) {
+                    for (int k = i; k >= 0; k--) {
                         if (dp[k][j-1]) {
                             isMatched = true;
                             break;
                         }
-                        k--;
                     }
                     
                     dp[i][j] = j == 1 || isMatched;
@@ -45,23 +43,21 @@ public class Solution {
     public boolean isMatch(String s, String p) {
         int sLen = s.length();
         int pLen = p.length();
-        
         boolean[][] dp = new boolean[sLen+1][pLen+1];
-        dp[sLen][pLen] = true;
         
-        for (int k = pLen - 1; k >= 0; k--) {
-            if (p.charAt(k) != '*') break;
-            dp[sLen][k] = true;
+        dp[sLen][pLen] = true;
+        for (int j = pLen - 1; j >= 0; j--) {
+            if (p.charAt(j) != '*') break;
+            dp[sLen][j] = true;
         }
         
         for (int i = sLen - 1; i >= 0; i--) {
             for (int j = pLen - 1; j >= 0; j--) {
-                if (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j)) {
-                    dp[i][j] = dp[i+1][j+1];
-                } else if (p.charAt(j) == '*') {
-                    dp[i][j] = dp[i+1][j] || dp[i][j+1];
+                char curr = p.charAt(j);
+                if (curr != '*') {
+                    dp[i][j] = dp[i+1][j+1] && (curr == '?' || curr == s.charAt(i));
                 } else {
-                    dp[i][j] = false;
+                    dp[i][j] = dp[i+1][j] || dp[i][j+1];
                 }
             }
         }
