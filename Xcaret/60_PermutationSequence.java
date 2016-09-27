1be662639a8efc8fa01dd5f3b207616bba1fd2dd
RuiLu <rlu0213@hotmail.com>
2016-09-26 20:59:48 -0400

    60

--- Xcaret/60_PermutationSequence.java
+++ Xcaret/60_PermutationSequence.java
@@ -4,24 +4,23 @@ public class Solution {
      *  Time complexity -> O(n)
      */
     public String getPermutation(int n, int k) {
-        int pos = 0;
         List<Integer> numbers = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
         int[] factorial = new int[n+1];
+        StringBuilder sb = new StringBuilder();
         
-        /* create an array for factorial number look-up */
         factorial[0] = 1;
         for (int i = 1; i <= n; i++) {
+            /* create an array for factorial number look-up */
             factorial[i] = factorial[i-1] * i;
-            
             /* create a list of numbers to get indices */
             numbers.add(i);
         }
         
+        /* we should deduct one, because we start from 0 */
         k--;
         
         for (int i = 1; i <= n; i++) {
-            int index = k/factorial[n-i];
+            int index = k / factorial[n-i];
             sb.append(numbers.get(index));
             numbers.remove(index);
             k -= index * factorial[n-i];
