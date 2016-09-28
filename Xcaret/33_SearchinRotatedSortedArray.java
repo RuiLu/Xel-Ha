ffd8aa4c477a30df78c37335bd2effd592bcc468
RuiLu <rlu0213@hotmail.com>
2016-09-27 23:42:15 -0400

    33

--- Xcaret/33_SearchinRotatedSortedArray.java
+++ Xcaret/33_SearchinRotatedSortedArray.java
@@ -1,19 +1,19 @@
 public class Solution {
     /**
-     *  Binary search
+     *  Binary search, we need to check the rotated situation.
      *  Time complexity -> O(logn)
      */
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return 0;
+        if (nums == null || nums.length == 0) return -1;
         
         int lo = 0;
         int hi = nums.length - 1;
         
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
-            
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) {
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
                 if (nums[lo] <= nums[mid] && nums[lo] > target) lo = mid + 1;
                 else hi = mid - 1;
             } else {
