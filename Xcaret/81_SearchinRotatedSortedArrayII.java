186c37ab84981b0c8ed7abbe34b0d031170367f7
RuiLu <rlu0213@hotmail.com>
2016-09-28 00:45:49 -0400

    81

--- Xcaret/81_SearchinRotatedSortedArrayII.java
+++ Xcaret/81_SearchinRotatedSortedArrayII.java
@@ -4,28 +4,22 @@ public class Solution {
      *  Time complexity -> O(logn)
      */
     public boolean search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return false;
-        }
+        if (nums == null || nums.length == 0) return false;
         
         int lo = 0;
         int hi = nums.length - 1;
         
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
-            
-            if (nums[mid] == target) {
-                return true;
-            }
-            
+            if (nums[mid] == target) return true;
             if (nums[mid] > nums[hi]) {
-                if (target > nums[mid] || target <= nums[hi]) lo = mid + 1;
+                if (nums[mid] > target && nums[lo] <= target) hi = mid - 1;
+                else lo = mid + 1;
+            } else if (nums[mid] < nums[hi]) {
+                if (nums[mid] < target && nums[hi] >= target) lo = mid + 1;
                 else hi = mid - 1;
-            } else if (nums[mid] == nums[hi]) {
-                hi--;
             } else {
-                if (target > nums[mid] && target <= nums[hi]) lo = mid + 1;
-                else hi = mid - 1;
+                hi--;
             }
         }
         
