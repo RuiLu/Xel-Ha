ad929a3e79a0c1ffbf425bdc3efb0d1ec966c810
RuiLu <rlu0213@hotmail.com>
2016-09-28 12:03:32 -0400

    81

--- Xcaret/81_SearchinRotatedSortedArrayII.java
+++ Xcaret/81_SearchinRotatedSortedArrayII.java
@@ -13,9 +13,11 @@ public class Solution {
             int mid = (lo + hi) / 2;
             if (nums[mid] == target) return true;
             if (nums[mid] > nums[hi]) {
+                /* in this situation, we can sure that the former part of array is monotonic increasing */
                 if (nums[mid] > target && nums[lo] <= target) hi = mid - 1;
                 else lo = mid + 1;
             } else if (nums[mid] < nums[hi]) {
+                /* in this situation, we can sure that the latter part of array is monotonic increasing */
                 if (nums[mid] < target && nums[hi] >= target) lo = mid + 1;
                 else hi = mid - 1;
             } else {
