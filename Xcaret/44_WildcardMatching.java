435c3b11a2bd9e2c25a908b48c9850d5ac26b2ec
RuiLu <rlu0213@hotmail.com>
2016-09-27 22:22:16 -0400

    44

--- Xcaret/44_WildcardMatching.java
+++ Xcaret/44_WildcardMatching.java
@@ -18,8 +18,8 @@ public class Solution {
                     /**
                      *  There are two situations: 
                      *  1. '*' appears at the first position of p, so dp[i][j] is true;
-                     *  2. '*' appears at the other positions in p, so we need to check previous result to see
-                     *     if there is any matching 
+                     *  2. '*' appears at the other positions in p, so we need to check former result to see
+                     *         if there is any matching
                      */
                     boolean isMatched = false;
                     for (int k = i; k >= 0; k--) {
@@ -28,7 +28,6 @@ public class Solution {
                             break;
                         }
                     }
-                    
                     dp[i][j] = j == 1 || isMatched;
                 }
             }
@@ -44,8 +43,8 @@ public class Solution {
         int sLen = s.length();
         int pLen = p.length();
         boolean[][] dp = new boolean[sLen+1][pLen+1];
-        
         dp[sLen][pLen] = true;
+        
         for (int j = pLen - 1; j >= 0; j--) {
             if (p.charAt(j) != '*') break;
             dp[sLen][j] = true;
@@ -53,11 +52,12 @@ public class Solution {
         
         for (int i = sLen - 1; i >= 0; i--) {
             for (int j = pLen - 1; j >= 0; j--) {
-                char curr = p.charAt(j);
-                if (curr != '*') {
-                    dp[i][j] = dp[i+1][j+1] && (curr == '?' || curr == s.charAt(i));
-                } else {
+                if (p.charAt(j) == '?' ||s.charAt(i) == p.charAt(j)) {
+                    dp[i][j] = dp[i+1][j+1];
+                } else if (p.charAt(j) == '*') {
                     dp[i][j] = dp[i+1][j] || dp[i][j+1];
+                } else {
+                    dp[i][j] = false;
                 }
             }
         }
