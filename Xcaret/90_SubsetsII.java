4682c63203ac644f2653b60ca29f28fb674185e0
RuiLu <rlu0213@hotmail.com>
2016-09-28 17:20:17 -0400

    90

---
+++ Xcaret/90_SubsetsII.java
@@ -0,0 +1,34 @@
+public class Solution {
+    /**
+     *  Backtracking
+     *  Time complexity -> O(2^n)
+     */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        if (nums == null || nums.length == 0) return res;
+        
+        Arrays.sort(nums);
+        boolean[] visited = new boolean[nums.length];
+        List<Integer> tmp = new ArrayList<>();
+        helper(res, nums, tmp, visited, 0);
+        
+        return res;
+    }
+    
+    private void helper(List<List<Integer>> res, int[] nums, List<Integer> tmp, boolean[] visited, int start) {
+        if (start == nums.length) return;
+        
+        for (int i = start; i < nums.length; i++) {
+            /* avoid duplicates */
+            if (i > 0 && !visited[i-1] && nums[i-1] == nums[i]) continue;
+            
+            tmp.add(nums[i]);
+            visited[i] = true;
+            res.add(new ArrayList<>(tmp));
+            helper(res, nums, tmp, visited, i + 1);
+            visited[i] = false;
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
