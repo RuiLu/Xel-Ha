f3be784b4458ca1c8c2f2c1cb1240b6381827376
RuiLu <rlu0213@hotmail.com>
2016-09-29 13:42:34 -0400

    87

--- Xcaret/87_ScrambleString.java
+++ Xcaret/87_ScrambleString.java
@@ -6,25 +6,26 @@ public class Solution {
      */
     public boolean isScramble(String s1, String s2) {
         if (s1.length() != s2.length()) return false;
+        
         if (s1.equals(s2)) return true;
         
-        /* first compare if two strings have the same number for characters */
-        int[] alphabet = new int[26];
+        /* 1. check if both strings contain same character */
+        int[] map = new int[26];
         for (int i = 0; i < s1.length(); i++) {
-            alphabet[s1.charAt(i) - 'a']++;
-            alphabet[s2.charAt(i) - 'a']--;
+            map[s1.charAt(i) - 'a']++;
+            map[s2.charAt(i) - 'a']--;
+        }
+        for (int val : map) {
+            if (val != 0) return false;
         }
         
-        for (int alpha : alphabet) {
-            if (alpha != 0) return false;
-        }
-        
-        /* then check all possible combinations */
+        /* 2. check all possible combinations and permutations */
         for (int i = 1; i < s1.length(); i++) {
+            /* i acts as a pivot. */
             if (isScramble(s1.substring(0, i), s2.substring(0, i)) &&
                 isScramble(s1.substring(i), s2.substring(i))) {
-                return true;        
-            } 
+                return true;
+            }
             if (isScramble(s1.substring(0, i), s2.substring(s2.length() - i)) &&
                 isScramble(s1.substring(i), s2.substring(0, s2.length() - i))) {
                 return true;
