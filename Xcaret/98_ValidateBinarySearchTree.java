a3c8d9751afaa9ea26f5cb174c4a2df115b05854
RuiLu <rlu0213@hotmail.com>
2016-09-29 14:27:16 -0400

    98

--- Xcaret/98_ValidateBinarySearchTree.java
+++ Xcaret/98_ValidateBinarySearchTree.java
@@ -15,39 +15,38 @@ public class Solution {
      */
     public boolean isValidBST(TreeNode root) {
         if (root == null) return true;
-        return isValidBST(root.left, Long.MIN_VALUE, root.val) &&
-              isValidBST(root.right, root.val, Long.MAX_VALUE);
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
     
-    public boolean isValidBST(TreeNode node, long min, long max) {
+    private boolean isValidBST(TreeNode node, long min, long max) {
         if (node == null) return true;
         
         if ((long)node.val <= min || (long)node.val >= max) return false;
         
-        return isValidBST(node.left, min, (long)node.val) &&
-              isValidBST(node.right, (long)node.val, max);
+        return isValidBST(node.left, min, (long)node.val) && 
+               isValidBST(node.right, (long)node.val, max);
     }
     
     /**
      *  Iteration, inorder
      */
+    
     public boolean isValidBST(TreeNode root) {
         if (root == null) return true;
         
+        TreeNode prev = null;
         Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
-        TreeNode left = null;
         
-        while (curr != null || !stack.isEmpty()) {
-            while (curr != null) {
-                stack.push(curr);
-                curr = curr.left;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
             }
             
             TreeNode node = stack.pop();
-            if (left != null && left.val >= node.val) return false;
-            left = node;
-            curr = node.right;
+            if (prev != null && prev.val >= node.val) return false;
+            prev = node;
+            root = node.right;
         }
         
         return true;
