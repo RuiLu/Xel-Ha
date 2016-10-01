77fadd9ce5f5459d6c8f8fca409441182f1d3214
RuiLu <rlu0213@hotmail.com>
2016-10-01 19:20:25 -0400

    114

---
+++ Xcaret/114_FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     *  Postorder traversal, starting from right subtree
+     *  Time complexity -> O(n)
+     *  Space complexity -> O(1)
+     */
+    private TreeNode prev = null; 
+    
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
