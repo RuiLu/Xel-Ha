681f24d43c8594353b07a8dca971e289d7ccb756
RuiLu <rlu0213@hotmail.com>
2016-10-01 20:00:15 -0400

    112

--- Xcaret/112_PathSum.java
+++ Xcaret/112_PathSum.java
@@ -8,12 +8,16 @@
  * }
  */
 public class Solution {
+    /**
+     *  Pay attention to a special test case [[], 0], which should return false
+     *  Time complexity -> O(2^h), where h is the height the tree
+     */
     public boolean hasPathSum(TreeNode root, int sum) {
         if (root == null) return false;
         if (root.left == null && root.right == null) {
             if (sum == root.val) return true;
             else return false;
         }
-        return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 }
