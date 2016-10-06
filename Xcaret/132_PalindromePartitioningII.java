0dc80b8bab78e6e8aba66891fb5ac4689930b136
RuiLu <rlu0213@hotmail.com>
2016-10-06 18:35:03 -0400

    132

--- Xcaret/132_PalindromePartitioningII.java
+++ Xcaret/132_PalindromePartitioningII.java
@@ -9,21 +9,21 @@ public class Solution {
         if (s == null || s.length() == 0) return 0;
         
         int len = s.length();
-        int[] dp = new int[len+1];
         char[] sca = s.toCharArray();
+        int[] dp = new int[len+1];
         
-        // set all values in dp to maximum possible value
+        // initialize dp array to possible maximum cut
         for (int i = 0; i <= len; i++) {
             dp[i] = i - 1;
         }
         
         for (int i = 0; i < len; i++) {
-            // check palindrome with odd length
+            // odd length
             for (int j = 0; i - j >= 0 && i + j < len && sca[i-j] == sca[i+j]; j++) {
                 dp[i+j+1] = Math.min(dp[i+j+1], dp[i-j] + 1);
             }
             
-            // check palindrome with even length
+            // even length 
             for (int j = 1; i - j + 1 >= 0 && i + j < len && sca[i-j+1] == sca[i+j]; j++) {
                 dp[i+j+1] = Math.min(dp[i+j+1], dp[i-j+1] + 1);
             }
