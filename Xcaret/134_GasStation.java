b185ac33b5874e1393bdfa4d6c7b1eea3367defd
RuiLu <rlu0213@hotmail.com>
2016-10-06 18:58:55 -0400

    134

--- Xcaret/134_GasStation.java
+++ Xcaret/134_GasStation.java
@@ -5,6 +5,8 @@ public class Solution {
      *  Space complexity -> O(1)
      */
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        if (gas == null || cost == null || gas.length != cost.length) return 0;
+        
         int start = gas.length - 1;
         int end = 0;
         int sum = gas[start] - cost[start];
@@ -27,39 +29,39 @@ public class Solution {
      *  Time complexity -> O(n^2)
      *  Space complexity -> O(1)
      */
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int len = gas.length;
+    // public int canCompleteCircuit(int[] gas, int[] cost) {
+    //     int len = gas.length;
         
-        // first, if total sum of gas is less than the total sum of cost, return -1
-        int diff = 0;
-        for (int i = 0; i < len; i++) {
-            diff += gas[i];
-            diff -= cost[i];
-        }
-        if (diff < 0) return -1;
+    //     // first, if total sum of gas is less than the total sum of cost, return -1
+    //     int diff = 0;
+    //     for (int i = 0; i < len; i++) {
+    //         diff += gas[i];
+    //         diff -= cost[i];
+    //     }
+    //     if (diff < 0) return -1;
         
-        // second, check availability one by one
-        for (int i = 0; i < len; i++) {
-            if (gas[i] < cost[i]) {
-                continue;
-            } else {
-                boolean failed = false;
-                int start = i;
-                int remain = gas[i] - cost[i];
-                i = (start + 1) % len;
-                while (i != start) {
-                    if (remain + gas[i] < cost[i]) {
-                        failed = true;
-                        break;
-                    } else {
-                        remain = remain + gas[i] - cost[i];
-                        i = (i + 1) % len;
-                    }
-                }
-                if (!failed) return start;
-            }
-        }
+    //     // second, check availability one by one
+    //     for (int i = 0; i < len; i++) {
+    //         if (gas[i] < cost[i]) {
+    //             continue;
+    //         } else {
+    //             boolean failed = false;
+    //             int start = i;
+    //             int remain = gas[i] - cost[i];
+    //             i = (start + 1) % len;
+    //             while (i != start) {
+    //                 if (remain + gas[i] < cost[i]) {
+    //                     failed = true;
+    //                     break;
+    //                 } else {
+    //                     remain = remain + gas[i] - cost[i];
+    //                     i = (i + 1) % len;
+    //                 }
+    //             }
+    //             if (!failed) return start;
+    //         }
+    //     }
         
-        return -1;
-    }
+    //     return -1;
+    // }
 }
