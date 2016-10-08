ccdf5f2c43e2c7ac9b874fe1bb84db98429e5adf
RuiLu <rlu0213@hotmail.com>
2016-10-08 15:20:35 -0400

    139

--- Xcaret/139_WordBreak.java
+++ Xcaret/139_WordBreak.java
@@ -5,41 +5,49 @@ public class Solution {
      *  Space complexity -> O(n)
      */
     public boolean wordBreak(String s, Set<String> wordDict) {
-        if (s == null || wordDict.size() == 0) return false;
+        if (s == null || s.length() == 0 || wordDict.size() == 0) return false;
         
         int len = s.length();
         boolean[] dp = new boolean[len+1];
         dp[0] = true;
         
         for (int i = 1; i <= len; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+            for (int left = 0; left < i; left++) {
+                if (dp[left] && wordDict.contains(s.substring(left, i))) {
                     dp[i] = true;
+                    break;
                 }
             }
         }
         
         return dp[len];
-    }
+    // }
     
     /**
      *  Backtracking
      */
     public boolean wordBreak(String s, Set<String> wordDict) {
-        if (s == null || wordDict.size() == 0) return false;
-        return helper(s, wordDict, 0, new HashSet<>());
+        if (s == null || s.length() == 0 || wordDict.size() == 0) return false;
+        Set<Integer> failed = new HashSet<>();
+        return helper(s, wordDict, 0, failed);
     }
     
     private boolean helper(String s, Set<String> wordDict, int start, Set<Integer> failed) {
-        if (start == s.length()) return true;
+        if (start == s.length()) {
+            return true;
+        }
         
-        if (failed.contains(start)) return false;
-   
+        if (failed.contains(start)) {
+            return false;
+        }
+        
         for (int i = start + 1; i <= s.length(); i++) {
             if (wordDict.contains(s.substring(start, i))) {
-                boolean res = helper(s, wordDict, i, failed);
-                if (res) return true;
-                else failed.add(i);
+                if (helper(s, wordDict, i, failed)) {
+                    return true;
+                } else {
+                    failed.add(start);
+                }
             }
         }
         
