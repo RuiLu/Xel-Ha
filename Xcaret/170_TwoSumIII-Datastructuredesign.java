5dd4ac49c604f3addfe05030b0c534ec3be6cfc2
RuiLu <rlu0213@hotmail.com>
2016-10-10 15:58:46 -0400

    170

---
+++ Xcaret/170_TwoSumIII-Datastructuredesign.java
@@ -0,0 +1,35 @@
+public class TwoSum {
+    /**
+     *  Using a Map and a List.
+     *  Key of map is number, value of map is the frequency of this number
+     *  Time complexity -> O(n)
+     */
+    private Map<Integer, Integer> map = new HashMap<>();
+    private List<Integer> list = new ArrayList<>();
+    
+    // Add the number to an internal data structure.
+	public void add(int number) {
+	    if (map.containsKey(number)) {
+	        map.put(number, map.get(number) + 1);
+	    } else {
+	        map.put(number, 1);
+	        list.add(number);
+	    }
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean find(int value) {
+	    for (int num : list) {
+	        int a = num;
+	        int b = value - num;
+	        if ((a == b && map.get(a) > 1) || (a != b && map.containsKey(b))) return true;
+	    }
+	    return false;
+	}
+}
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum = new TwoSum();
+// twoSum.add(number);
+// twoSum.find(value);
