f146182920c23c8dce3a0564a73fb022e57afe20
RuiLu <rlu0213@hotmail.com>
2016-10-10 11:55:01 -0400

    356

--- Xcaret/356_LineReflection.java
+++ Xcaret/356_LineReflection.java
@@ -8,22 +8,23 @@ public class Solution {
      *  Space complexity -> O(n)
      */
     public boolean isReflected(int[][] points) {
-        if (points == null || points.length < 1) return false;
-        if (points.length == 1) return true;
+        if (points == null || points.length <= 1) return true;
         
         Set<String> set = new HashSet<>();
-        int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
         
+        // calculate maximum x value and minimum x value, the id is in the format of "x-y"
         for (int[] point : points) {
-            max = Math.max(max, point[0]);
             min = Math.min(min, point[0]);
+            max = Math.max(max, point[0]);
             String id = point[0] + "-" + point[1];
             set.add(id);
         }
         
         int sum = max + min;
         
+        // if there is a reflection, "(sum-x)-y" will also in Set
         for (int[] point : points) {
             String id = (sum - point[0]) + "-" + point[1];
             if (!set.contains(id)) return false;
@@ -32,3 +33,4 @@ public class Solution {
         return true;
     }
 }
+
