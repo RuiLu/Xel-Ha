70b7ac78ca95c5481b9d5c97f015c703c5338a8c
RuiLu <rlu0213@hotmail.com>
2016-10-10 10:31:35 -0400

    159

--- Xcaret/159_LongestSubstringwithAtMostTwoDistinctCharacters.java
+++ Xcaret/159_LongestSubstringwithAtMostTwoDistinctCharacters.java
@@ -4,38 +4,37 @@ public class Solution {
      *  Time complexity -> O(n) , in worst case -> O(n^2)
      */
     public int lengthOfLongestSubstringTwoDistinct(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
+        if (s == null || s.length() == 0) return 0;
         
-        int counter = 0;
-        int max = Integer.MIN_VALUE;
-        int[] map = new int[128];
         char[] sca = s.toCharArray();
+        int[] map = new int[128];
+        int counter = 0;
         int begin = 0;
         int end = 0;
+        int maxLen = 0;
         
-        while (end < sca.length) {
-            /* meet a new character, we add one to counter */
+        while(end < sca.length) {
+            // meet a new character, we add one to counter
             if (map[sca[end]] == 0) {
                 counter++;
             }
+            
             map[sca[end]]++;
             end++;
             
-            /* when there are 3 distinct characters */
+            // when there are 3 distinct characters
             while (counter > 2) {
                 map[sca[begin]]--;
-                /* map[sca[begin]] == 0 means that we have taken out one characters from current range */
+                // map[sca[begin]] == 0 means that we have taken out one characters from current range
                 if (map[sca[begin]] == 0) {
                     counter--;
                 }
                 begin++;
             }
             
-            max = Math.max(max, end - begin);
+            maxLen = Math.max(maxLen, end - begin);   
         }
         
-        return max;
+        return maxLen;
     }
 }
