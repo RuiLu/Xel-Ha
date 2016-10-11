83fa7ab51ff8388e6edd549f268e1d520dc819bf
RuiLu <rlu0213@hotmail.com>
2016-10-10 22:55:14 -0400

    179

--- Xcaret/179_LargestNumber.java
+++ Xcaret/179_LargestNumber.java
@@ -2,35 +2,36 @@ public class Solution {
     /**
      *  Directly compare the combination of two strings, and sorted them in descending order
      *  Let's assume n is the length of nums, k is average length of number string
-     *        1. compare two strings takes O(k)
-     *        2. sorted n strings takes O(nklognk)
-     *        3. assign to StringBuilder takes O(n)
+     *        1. compare two strings takes O(k), where k is the length of string
+     *        2. sort a string takes O(klogk)
+     *        3. sorted n strings takes O(nklognk)
+     *        4. assign to StringBuilder takes O(n)
      *  So the total time is O(nklognk)
      */
     public String largestNumber(int[] nums) {
         if (nums == null || nums.length == 0) return "";
         
-        int len = nums.length;
-        String[] strs = new String[len];
         StringBuilder sb = new StringBuilder();
+        String[] tokens = new String[nums.length];
         
-        for (int i = 0; i < len; i++) {
-            strs[i] = String.valueOf(nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            tokens[i] = Integer.toString(nums[i]);
         }
         
-        Arrays.sort(strs, new Comparator<String>(){
-            public int compare(String s1, String s2) {
-                String sum1 = s1 + s2;
-                String sum2 = s2 + s1;
-                return sum2.compareTo(sum1);
-            }
+        // sort tokens by comparing combined string with each other, result is in descending order
+        Arrays.sort(tokens, new Comparator<String>(){
+           public int compare(String s1, String s2) {
+              String sum1 = s1 + s2;
+              String sum2 = s2 + s1;
+              return sum2.compareTo(sum1);
+           } 
         });
         
-        /* We should pay attention to the all-zero situation */
-        if (strs[0].charAt(0) == '0') return "0";
+        // if elements in nums array are all 0, return 0
+        if (tokens[0].charAt(0) == '0') return "0";
         
-        for (String str : strs) {
-            sb.append(str);
+        for (String token : tokens) {
+            sb.append(token);
         }
         
         return sb.toString();
