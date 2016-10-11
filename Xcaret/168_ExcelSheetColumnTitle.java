098c4bd14ba67d8cb7441cf84f0073000025dd33
RuiLu <rlu0213@hotmail.com>
2016-10-11 16:20:55 -0400

    168

--- Xcaret/168_ExcelSheetColumnTitle.java
+++ Xcaret/168_ExcelSheetColumnTitle.java
@@ -3,18 +3,19 @@ public class Solution {
      *  Time complexity -> O(log26(n)) -> base of log is 26
      */
     public String convertToTitle(int n) {
-        if (n <= 0) return null;
+        if (n <= 0) return "";
+        
         StringBuilder sb = new StringBuilder();
         
         while (n != 0) {
-            int remainder = n % 26 - 1;
-            n /= 26;
-            if (remainder == -1) {
+            int value = n % 26;
+            if (value == 0) {
                 sb.insert(0, 'Z');
                 n--;
             } else {
-                sb.insert(0, (char)('A' + remainder));
+                sb.insert(0, (char)('A' + value - 1));
             }
+            n /= 26;
         }
         
         return sb.toString();
