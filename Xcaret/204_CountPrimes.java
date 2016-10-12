c7ed64f224201bb4f5a2bdcc72688426485976ab
RuiLu <rlu0213@hotmail.com>
2016-10-12 19:35:39 -0400

    204

--- Xcaret/204_CountPrimes.java
+++ Xcaret/204_CountPrimes.java
@@ -8,14 +8,15 @@ public class Solution {
     public int countPrimes(int n) {
         if (n <= 2) return 0;
         
-        boolean[] notPrime = new boolean[n];
         int res = 0;
+        boolean[] notPrime = new boolean[n];
         
         for (int i = 2; i < n; i++) {
             if (notPrime[i]) continue;
             
             res++;
             int iter = i;
+            
             while (iter < n) {
                 notPrime[iter] = true;
                 iter += i;
