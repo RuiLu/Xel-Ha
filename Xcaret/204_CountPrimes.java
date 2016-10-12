95cafb9b652b74b009635195b47394fdc03b225c
RuiLu <rlu0213@hotmail.com>
2016-10-12 11:49:31 -0400

    204

---
+++ Xcaret/204_CountPrimes.java
@@ -0,0 +1,27 @@
+public class Solution {
+    /**
+     *  Idea -> Sieve of Eratosthenes
+     *          * if we use set, Still Time Limited Exceeded...
+     *          * so we need to use a boolean array to work as a set
+     *  Reference -> https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+     */
+    public int countPrimes(int n) {
+        if (n <= 2) return 0;
+        
+        boolean[] notPrime = new boolean[n];
+        int res = 0;
+        
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i]) continue;
+            
+            res++;
+            int iter = i;
+            while (iter < n) {
+                notPrime[iter] = true;
+                iter += i;
+            }
+        }
+        
+        return res;
+    }
+}
