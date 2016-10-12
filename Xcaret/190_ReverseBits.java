09cd948ca12f9d05a6cc9593a62a36f6aba0e822
RuiLu <rlu0213@hotmail.com>
2016-10-12 00:51:37 -0400

    190

---
+++ Xcaret/190_ReverseBits.java
@@ -0,0 +1,20 @@
+public class Solution {
+    /**
+     *  Idea -> At every round, we left shift res one bit.
+     *          If lowest bit of n is 1, we add 1 to res, otherwise, add 0 to res.
+     *          Then right shift n one bit.
+     *  Time complexity -> O(32)
+     *  Space complexity -> O(1)
+     */
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        if (n == 0) return n;
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res += (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+}
