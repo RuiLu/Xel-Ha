0b94b0400890a3c33b4591224c7ba49c2cce47b1
RuiLu <rlu0213@hotmail.com>
2016-10-12 23:56:02 -0400

    201

--- Xcaret/201_BitwiseANDofNumbersRange.java
+++ Xcaret/201_BitwiseANDofNumbersRange.java
@@ -3,7 +3,7 @@ public class Solution {
      *  Idea -> 1. The last bit of (odd number & even number) is always 0
      *          2. when m != n, there is at least an odd number and an even number, so the last bit is 0
      *          3. when m != n, right shift m and n one bit, and left shift moveFactor one bit, until m == n
-     *  Time complexity -> O(32)
+     *  Time complexity -> O(32) -> O(1)
      *  Reference -> https://discuss.leetcode.com/topic/12133/bit-operation-solution-java
      */
     public int rangeBitwiseAnd(int m, int n) {
