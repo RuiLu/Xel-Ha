020010a1afec32d72c7e52ed811ea621186d733a
RuiLu <rlu0213@hotmail.com>
2016-10-19 15:29:49 -0400

    214

--- Xcaret/214_ShortestPalindrome.java
+++ Xcaret/214_ShortestPalindrome.java
@@ -5,13 +5,12 @@ public class Solution {
      *  Time complexity -> O(n)
      */
     private int[] getTable(String s) {
-        int len = s.length();
-        int[] table = new int[len+1];
-        int i = 0; 
+        int[] table = new int[s.length()+1];
+        int i = 0;
         int j = -1;
         table[i] = j;
         
-        while (i < len) {
+        while (i < s.length()) {
             while (j >= 0 && s.charAt(i) != s.charAt(j)) j = table[j];
             i++;
             j++;
@@ -19,13 +18,12 @@ public class Solution {
         }
         
         return table;
-    } 
-     
+    }
+
     public String shortestPalindrome(String s) {
-        if (s == null || s.length() <= 1) return s;
+        if (s == null || s.length() == 0) return s;
+        
         int[] table = getTable(s + "#" + new StringBuilder(s).reverse().toString());
-        // if you want to see what's inside the table
-        // for (int i : table) System.out.print(i + " ");
         return new StringBuilder(s.substring(table[table.length - 1])).reverse().toString() + s;
     }
     
@@ -36,11 +34,9 @@ public class Solution {
     public String shortestPalindrome(String s) {
         int j = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == s.charAt(j)) j++;
+            if (s.charAt(j) == s.charAt(i)) j++;
         }
-        
-        if (j == s.length()) return s;
-        
+        if (s.length() == j) return s;
         String suffix = s.substring(j);
         return new StringBuilder(suffix).reverse().toString() + shortestPalindrome(s.substring(0, j)) + suffix;
     }
