9ca103de5b5dd9ba9b721371299a83f1834731f1
RuiLu <rlu0213@hotmail.com>
2016-10-20 00:48:08 -0400

    22

--- Xcaret/221_MaximalSquare.java
+++ Xcaret/221_MaximalSquare.java
@@ -1,17 +1,17 @@
 public class Solution {
     /**
      *  Idea -> Dynamic Programming
-     *          Keeping track of maximal length of side!
+     *          Keeping track of maximal length of side!!
      *  Time complexity -> O(mn)
      *  Space complexity -> O(mn)
      */
     public int maximalSquare(char[][] matrix) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
         
-        int maxLen = 0;
         int row = matrix.length;
         int col = matrix[0].length;
         int[][] dp = new int[row][col];
+        int maxLen = 0;
         
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
@@ -19,7 +19,7 @@ public class Solution {
                     if (i == 0 || j == 0) {
                         dp[i][j] = 1;
                     } else {
-                        dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                        dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
                     }
                     maxLen = Math.max(maxLen, dp[i][j]);
                 }
