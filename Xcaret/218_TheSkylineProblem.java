46e3ab413168337870bc50e5a6070fd10702a077
RuiLu <rlu0213@hotmail.com>
2016-10-20 22:26:38 -0400

    218

--- Xcaret/218_TheSkylineProblem.java
+++ Xcaret/218_TheSkylineProblem.java
@@ -6,41 +6,38 @@ public class Solution {
      *  Space complexity -> O(n)
      */
     public List<int[]> getSkyline(int[][] buildings) {
-        if (buildings == null || buildings.length == 0 || buildings[0].length != 3) {
+        if (buildings == null || buildings.length == 0 || buildings[0].length == 0) {
             return new ArrayList<int[]>();
         }
         
-        List<int[]> heights = new ArrayList<>();
-        List<int[]> skylines = new ArrayList<>();
+        List<int[]> heights = new ArrayList<int[]>();
+        List<int[]> skylines = new ArrayList<int[]>();
         
         for (int[] building : buildings) {
             heights.add(new int[]{building[0], -building[2]});
             heights.add(new int[]{building[1], building[2]});
         }
         
-        // sorting the heights in ascending order according to heights[0]
-        Collections.sort(heights, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        Collections.sort(heights, (a, b) -> (a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]));
         
-        TreeMap<Integer, Integer> tmap = new TreeMap<>();
-        // we need to put a default value in it
+        TreeMap<Integer, Integer> tmap = new TreeMap<Integer, Integer>();
         tmap.put(0, -1);
-        int previous = 0;
+        int prevHeight = 0;
         
         for (int[] height : heights) {
             if (height[1] < 0) {
-                Integer num = tmap.get(-height[1]);
-                num = num == null ? 1 : num + 1;
-                tmap.put(-height[1], num);
+                if (!tmap.containsKey(-height[1])) tmap.put(-height[1], 0);
+                tmap.put(-height[1], tmap.get(-height[1]) + 1);
             } else {
-                Integer num = tmap.get(height[1]);
-                if (num == 1) tmap.remove(height[1]);
-                else tmap.put(height[1], num - 1);
+                tmap.put(height[1], tmap.get(height[1]) - 1);
+                if (tmap.get(height[1]) == 0) tmap.remove(height[1]);
             }
             
-            int current = tmap.lastKey();
-            if (previous != current) {
-                skylines.add(new int[]{height[0], current});
-                previous = current;
+            int currHeight = tmap.lastKey();
+            
+            if (prevHeight != currHeight) {
+                skylines.add(new int[]{height[0], currHeight});
+                prevHeight = currHeight;
             }
         }
         
