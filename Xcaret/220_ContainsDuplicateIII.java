0531cd68030fdb41222c0accdbaeb83f5d45c700
RuiLu <rlu0213@hotmail.com>
2016-10-20 23:10:28 -0400

    220

--- Xcaret/220_ContainsDuplicateIII.java
+++ Xcaret/220_ContainsDuplicateIII.java
@@ -4,37 +4,22 @@ public class Solution {
      *  Time complexity -> O(nlogk)
      */
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (nums == null || nums.length <= 1 || k < 1 || t < 0) return false;
+        if (k < 1 || t < 0 || nums == null || nums.length <= 1) return false;
+        
         TreeSet<Integer> tset = new TreeSet<Integer>();
+        
         for (int i = 0; i < nums.length; i++) {
             Integer floor = tset.ceiling(nums[i] - t);
             Integer ceiling = tset.floor(nums[i] + t);
-            if ((floor != null && floor <= nums[i]) || (ceiling != null && ceiling >= nums[i])) {
+            
+            if ((floor != null && nums[i] >= floor) || (ceiling != null && nums[i] <= ceiling)) {
                 return true;
             }
+            
             tset.add(nums[i]);
-            if (i >= k) tset.remove(nums[i-k]);
+            if (i >= k) tset.remove(nums[i-k]);    
         }
+        
         return false;
     }
-    
-    /**
-     *  Time Limit Exceeded
-     *  Idea -> Take the advantage of sliding window
-     *  Time complexity -> O(nt)
-     *  Space complexity -> O(K)
-     */
-    // public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-    //     if (nums == null || nums.length <= 1 || t < 0) return false;
-    //     Set<Integer> set = new HashSet<Integer>();
-    //     t = Math.abs(t);
-    //     for (int i = 0; i < nums.length; i++) {
-    //         if (i > k) set.remove(nums[i-k-1]);
-    //         for (int j = nums[i] - t; j <= nums[i] + t; j++) {
-    //             if (set.contains(j)) return true;
-    //         }
-    //         set.add(nums[i]);
-    //     }
-    //     return false;
-    // }
 }
