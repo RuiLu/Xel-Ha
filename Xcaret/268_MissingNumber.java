326405fdfe6a3fb764a1fee59c58a08822e9bb4d
RuiLu <rlu0213@hotmail.com>
2016-10-22 16:30:53 -0400

    268

---
+++ Xcaret/268_MissingNumber.java
@@ -0,0 +1,26 @@
+public class Solution {
+    /**
+     *  Idea -> put every number into the right position, like nums[i] = i
+     *          then traverse the array to find anomaly
+     *  Time complexity -> O(n)
+     *  Space complexity -> O(1)
+     */
+    public int missingNumber(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == i || nums[i] == nums.length) continue;
+            while (nums[i] != i && nums[i] != nums.length) {
+                int tmp = nums[nums[i]];
+                nums[nums[i]] = nums[i];
+                nums[i] = tmp;
+            }
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) return i;
+        }
+        
+        return nums.length;
+    }
+}
