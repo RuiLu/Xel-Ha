c3b5605d33f05b133897c7bac590e8eabfb470c6
RuiLu <rlu0213@hotmail.com>
2016-10-22 13:15:11 -0400

    223

--- Xcaret/223_RectangleArea.java
+++ Xcaret/223_RectangleArea.java
@@ -6,13 +6,13 @@ public class Solution {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
         int areaA = (C - A) * (D - B);
         int areaB = (G - E) * (H - F);
-
+        
         int left = Math.max(A, E);
         int right = Math.min(C, G);
         int up = Math.min(D, H);
         int down = Math.max(B, F);
         
-        // if there is no overlap, then reduce 0
+        // if there is an overlap, then just deduct 0 
         int overlap = 0;
         if (right > left && up > down) {
             overlap = (right - left) * (up - down);
