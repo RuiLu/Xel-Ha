99b899aba0de9df79ed93a5b28edd26f424d4a00
RuiLu <rlu0213@hotmail.com>
2016-10-22 23:47:50 -0400

    419

---
+++ Xcaret/419_BattleshipsinaBoard.java
@@ -0,0 +1,81 @@
+public class Solution {
+    /**
+     *  Idea -> No need to detect whether the board is valid
+     *          Count directly, if there is ship before the line, vertically or horizontally, continue
+     *  Time complexity -> O(mn)
+     *  Space complexity -> O(1)
+     */ 
+    public int countBattleships(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) return 0;
+        
+        int res = 0;
+        int row = board.length;
+        int col = board[0].length;
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == '.') continue;
+                if (i > 0 && board[i-1][j] == 'X') continue;
+                if (j > 0 && board[i][j-1] == 'X') continue;
+                res++;
+            }
+        }
+        
+        return res;
+    }    
+    
+    /**
+     *  Idea -> First to detect where the board is valid or not
+     *          Then count
+     */
+    public int countBattleships(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) return 0;
+        
+        int res = 0;
+        int row = board.length;
+        int col = board[0].length;
+        
+        for (int i = 0; i < row; i++) {
+            boolean horizontal = false;
+            boolean vertical = false;
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 'X') {
+                    if (j > 0 && board[i][j] == board[i][j-1]) {
+                        horizontal = true;
+                        if (vertical) return 0;
+                    }
+                    if ((i > 0 && board[i-1][j] == 'X') || (i < row - 1 && board[i+1][j] == 'X')) {
+                        vertical = true;
+                        if (horizontal) return 0;
+                    }
+                } else {
+                    horizontal = false;
+                    vertical = false;
+                }
+            }
+        }
+        
+        boolean[][] visited = new boolean[row][col];
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (!visited[i][j] && board[i][j] == 'X') {
+                    res++;
+                    dfs(board, i, j, visited);
+                }
+            }
+        }
+        
+        return res;
+    }
+
+    private void dfs(char[][] board, int i, int j, boolean[][] visited) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j] || board[i][j] != 'X') return;
+        
+        visited[i][j] = true;
+        dfs(board, i - 1, j, visited);
+        dfs(board, i + 1, j, visited);
+        dfs(board, i, j - 1, visited);
+        dfs(board, i, j + 1, visited);
+    }
+}
