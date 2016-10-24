04752a1d3f35aa55c10b8314ca2a3d446bbd9c74
RuiLu <rlu0213@hotmail.com>
2016-10-23 23:34:38 -0400

    419

--- Xcaret/419_BattleshipsinaBoard.java
+++ Xcaret/419_BattleshipsinaBoard.java
@@ -7,21 +7,17 @@ public class Solution {
      */ 
     public int countBattleships(char[][] board) {
         if (board == null || board.length == 0 || board[0].length == 0) return 0;
-        
-        int res = 0;
-        int row = board.length;
-        int col = board[0].length;
-        
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (board[i][j] == '.') continue;
-                if (i > 0 && board[i-1][j] == 'X') continue;
-                if (j > 0 && board[i][j-1] == 'X') continue;
-                res++;
+        int count = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'X') {
+                    if (j > 0 && board[i][j] == board[i][j-1]) continue;
+                    if (i > 0 && board[i][j] == board[i-1][j]) continue;
+                    count++;
+                }
             }
         }
-        
-        return res;
+        return count;
     }    
     
     /**
@@ -69,13 +65,13 @@ public class Solution {
         return res;
     }
 
-    private void dfs(char[][] board, int i, int j, boolean[][] visited) {
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j] || board[i][j] != 'X') return;
+    // private void dfs(char[][] board, int i, int j, boolean[][] visited) {
+    //     if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j] || board[i][j] != 'X') return;
         
-        visited[i][j] = true;
-        dfs(board, i - 1, j, visited);
-        dfs(board, i + 1, j, visited);
-        dfs(board, i, j - 1, visited);
-        dfs(board, i, j + 1, visited);
-    }
+    //     visited[i][j] = true;
+    //     dfs(board, i - 1, j, visited);
+    //     dfs(board, i + 1, j, visited);
+    //     dfs(board, i, j - 1, visited);
+    //     dfs(board, i, j + 1, visited);
+    // }
 }
