882acb25cceea3a312fffe2296803111c9707fdc
RuiLu <rlu0213@hotmail.com>
2016-10-25 19:25:05 -0400

    54

--- Xcaret/54_SpiralMatrix.java
+++ Xcaret/54_SpiralMatrix.java
@@ -4,36 +4,35 @@ public class Solution {
      *  the time complexity is O(mn)
      */
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return res;
+        List<Integer> list = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return list;
         
-        int row = matrix.length;
-        int col = matrix[0].length;
         int x = 0;
         int y = 0;
+        int row = matrix.length;
+        int col = matrix[0].length;
         
         while (row > 0 && col > 0) {
             if (row == 1) {
-                for (int k = 0; k < col; k++) res.add(matrix[x][y++]);
+                for (int i = 0; i < col; i++) list.add(matrix[x][y++]);
                 break;
             }
-            
             if (col == 1) {
-                for (int k = 0; k < row; k++) res.add(matrix[x++][y]);
+                for (int i = 0; i < row; i++) list.add(matrix[x++][y]);
                 break;
             }
             
-            for (int k = 0; k < col - 1; k++) res.add(matrix[x][y++]);
-            for (int k = 0; k < row - 1; k++) res.add(matrix[x++][y]);
-            for (int k = 0; k < col - 1; k++) res.add(matrix[x][y--]);
-            for (int k = 0; k < row - 1; k++) res.add(matrix[x--][y]);
+            for (int i = 0; i < col - 1; i++) list.add(matrix[x][y++]);
+            for (int i = 0; i < row - 1; i++) list.add(matrix[x++][y]);
+            for (int i = 0; i < col - 1; i++) list.add(matrix[x][y--]);
+            for (int i = 0; i < row - 1; i++) list.add(matrix[x--][y]);
             
+            x += 1;
+            y += 1;
             row -= 2;
             col -= 2;
-            x++;
-            y++;
         }
         
-        return res;
+        return list;
     }
 }
