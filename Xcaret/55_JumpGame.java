6e7b081688a9df5085ca39b0807fed8152038ccc
RuiLu <rlu0213@hotmail.com>
2016-10-26 16:54:14 -0400

    55

--- Xcaret/55_JumpGame.java
+++ Xcaret/55_JumpGame.java
@@ -1,37 +1,35 @@
 public class Solution {
     /**
-     *  Greedy
-     *  Time complexity -> O(n)
-     */
-    public boolean canJump(int[] nums) {
-        if (nums == null || nums.length == 0) return false;
-        
-        int max = 0;
-        int foot = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            max = Integer.max(max, i + nums[i]);
-            if (i == foot) {
-                foot = max;
-            }
-            if (foot >= nums.length - 1) return true;
-        }
-        
-        return false;
-    }
-    
-    /**
      *  Backward
      *  Time complexity -> O(n)
      */
     public boolean canJump(int[] nums) {
-        if (nums == null || nums.length == 0) return false;
+        if (nums == null || nums.length <= 1) return true;
+        
         int last = nums.length - 1;
         
         for (int i = nums.length - 2; i >= 0; i--) {
             if (i + nums[i] >= last) last = i;
         }
         
-        return last <= 0;
+        return last == 0;
+    }
+    
+    /**
+     *  Greedy
+     *  Time complexity -> O(n)
+     */
+    public boolean canJump(int[] nums) {
+        if (nums == null || nums.length <= 1) return true;
+        
+        int maxReach = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) return true;
+            if (maxReach <= i) return false;
+        }
+        
+        return false;
     }
 }
