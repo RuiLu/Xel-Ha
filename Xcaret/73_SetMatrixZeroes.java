c02a9c370b559f6c08608e576feaee4f480d4464
RuiLu <rlu0213@hotmail.com>
2016-10-26 22:22:59 -0400

    73

--- Xcaret/73_SetMatrixZeroes.java
+++ Xcaret/73_SetMatrixZeroes.java
@@ -1,5 +1,49 @@
 public class Solution {
     /**
+     *  Idea -> We can use row-0 and col-0 to store 
+     *          Attention: we need to take the first row and first col out to take into consideration seperately.
+     *  Time complexity -> O(mn)
+     *  Space complexity -> O(1)
+     */
+    public void setZeroes(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
+        boolean firstRow = false;
+        boolean firstCol = false;
+         
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if (i == 0) firstRow = true;
+                    if (j == 0) firstCol = true;
+                }
+            }
+        }
+        
+        for (int i = 1; i < row; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < col; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 1; j < col; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < row; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if (firstRow) for (int j = 0; j < col; j++) matrix[0][j] = 0;
+        if (firstCol) for (int i = 0; i < row; i++) matrix[i][0] = 0;
+    } 
+     
+    /**
      *  Using two sets to record the position whose value is zero,
      *  then fill out these rows and cols with zero
      *  Time complexity -> O(mn)
@@ -13,7 +57,7 @@ public class Solution {
         int row = matrix.length;
         int col = matrix[0].length;
         
-        for (int i = 0; i< row; i++) {
+        for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (matrix[i][j] == 0) {
                     rows.add(i);
@@ -22,16 +66,11 @@ public class Solution {
             }
         }
         
-        for (int i : rows) {
-            for (int j = 0; j < col; j++) {
-                matrix[i][j] = 0;
-            }
+        for (int r : rows) {
+            for (int i = 0; i < col; i++) matrix[r][i] = 0;
         }
-        
-        for (int j : cols) {
-            for (int i = 0; i < row; i++) {
-                matrix[i][j] = 0;
-            }
+        for (int c : cols) {
+            for (int i = 0; i < row; i++) matrix[i][c] = 0;
         }
     }
 }
