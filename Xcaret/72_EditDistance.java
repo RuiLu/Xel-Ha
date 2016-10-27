815ccd1a3c82336ec136156a7254a08d446e4db4
RuiLu <rlu0213@hotmail.com>
2016-10-26 23:12:33 -0400

    72

--- Xcaret/72_EditDistance.java
+++ Xcaret/72_EditDistance.java
@@ -8,22 +8,20 @@ public class Solution {
         int len2 = word2.length();
         int[][] dp = new int[len1+1][len2+1];
         
-        /* initialize the distance array */
-        for (int i = 0; i <= len1; i++) {
-            dp[i][0] = i;
-        }
-        for (int j = 0; j <= len2; j++) {
-            dp[0][j] = j;
-        }
+        for (int i = 0; i <= len1; i++) dp[i][0] = i;
+        for (int j = 0; j <= len2; j++) dp[0][j] = j;
         
         for (int i = 1; i <= len1; i++) {
             for (int j = 1; j <= len2; j++) {
-                if (word1.charAt(i-1) == word2.charAt(j-1)) {
-                    /* no need for any operation */
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i-1][j-1];
                 } else {
-                    /* 1. insert->dp[i][j-1]; 2. delete->dp[i-1][j]; 3. replace->dp[i-1][j-1] */
-                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    /* dp[i-1][j-1] -> replace
+                     * dp[i-1][j] -> delete
+                     * dp[i][j-1] -> insert
+                     * All operations are on word1
+                     */
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
                 }
             }
         }
