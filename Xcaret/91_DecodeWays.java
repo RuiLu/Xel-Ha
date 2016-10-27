52c8fade50460fae9e271a2ee498231186ec0bf4
RuiLu <rlu0213@hotmail.com>
2016-10-26 21:54:52 -0400

    91

--- Xcaret/91_DecodeWays.java
+++ Xcaret/91_DecodeWays.java
@@ -2,28 +2,30 @@ public class Solution {
     /**
      *  Dynamic Programming
      *  Time complexity -> O(n)
+     *  Space complexity -> O(n)
      */
+    private boolean isValid(String s) {
+        if (s.length() == 1) {
+            if (s.equals("0")) return false;
+            return true;
+        }
+        
+        return Integer.parseInt(s) >= 10 && Integer.parseInt(s) <= 26;
+    } 
+     
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
+        if (s == null || s.length() == 0) return 0;
         
         int len = s.length();
         int[] dp = new int[len+1];
-        
         dp[0] = 1;
-        if (isValid(s.substring(0, 1))) dp[1] = 1;
-        else dp[1] = 0;
         
-        for (int i = 2; i <= len; i++) {
-            if (isValid(s.substring(i-1, i))) dp[i] += dp[i-1];
-            if (isValid(s.substring(i-2, i))) dp[i] += dp[i-2];
+        for (int i = 1; i <= len; i++) {
+            if (isValid(s.substring(i - 1, i))) dp[i] += dp[i-1];
+            if (i > 1 && isValid(s.substring(i - 2, i))) dp[i] += dp[i-2];
         }
         
         return dp[len];
     }
     
-    private boolean isValid(String s) {
-        if (s.charAt(0) == '0') return false;
-        int value = Integer.parseInt(s);
-        return value >= 1 && value <= 26;
-    }
 }
