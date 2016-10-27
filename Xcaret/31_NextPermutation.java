12e640c5fdb3446954137820e2d3f676dffe6946
RuiLu <rlu0213@hotmail.com>
2016-10-26 21:08:57 -0400

    31

--- Xcaret/31_NextPermutation.java
+++ Xcaret/31_NextPermutation.java
@@ -1,33 +1,37 @@
 public class Solution {
     /**
      *  Search the array from right to left to find the first element that nums[i] < nums[i+1]
-     *  Then find a element from i + 1 to len - 1 whose value is less or equal to nums[i], swap
+     *  Then find a element from i + 1 to len - 1 whose value is less or equal to nums[i], swap its previous element
      *  Finally, reverse the order from i + 1 to len - 1
      */
+    private void reverse(int[] nums, int lo, int hi) {
+        while (lo < hi) {
+            swap(nums, lo, hi);
+            lo++;
+            hi--;
+        }
+    }     
+     
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    } 
+     
     public void nextPermutation(int[] nums) {
         if (nums == null || nums.length <= 1) return;
         
         int i = nums.length - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+        
+        while (i >= 0 && nums[i] >= nums[i+1]) i--;
         
         if (i != -1) {
             int j = i + 1;
             while (j < nums.length && nums[i] < nums[j]) j++;
             j--;
-            
             swap(nums, i, j);
         }
         
         reverse(nums, i + 1, nums.length - 1);
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
-    }
-    
-    private void reverse(int[] nums, int i, int j) {
-        while (i < j) swap(nums, i++, j--);
     }
 }
