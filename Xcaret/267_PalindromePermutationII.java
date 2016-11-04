e522abdec6072bb9ff8e3952388361274d869562
RuiLu <rlu0213@hotmail.com>
2016-11-04 10:43:33 -0400

    267

--- Xcaret/267_PalindromePermutationII.java
+++ Xcaret/267_PalindromePermutationII.java
@@ -4,45 +4,46 @@ public class Solution {
      *          Otherwise, if the lengthe is odd, find the mid, and construct palindrome from both sides of mid.
      *  Time complexity -> O((n/2)^(n/2)) in worst case, where n is the length of String
      */
-    private static void getAllPermutations(List<String> res, Map<Character, Integer> map, String mid, int len) {
+    private static void getAllPermutations(Map<Character, Integer> map, List<String> res, String mid, int len) {
         if (mid.length() == len) {
             res.add(mid);
         } else {
-            for (char ch : map.keySet()) {
-                if (map.get(ch) <= 0) continue;
-                mid = ch + mid + ch;
-                map.put(ch, map.get(ch) - 2);
-                getAllPermutations(res, map, mid, len);
+            for (Map.Entry<Character, Integer> me : map.entrySet()) {
+                if (me.getValue() <= 0) continue;
+                map.put(me.getKey(), me.getValue() - 2);
+                mid = me.getKey() + mid + me.getKey();
+                getAllPermutations(map, res, mid, len);
                 mid = mid.substring(1, mid.length() - 1);
-                map.put(ch, map.get(ch) + 2);
+                map.put(me.getKey(), me.getValue() + 2);
             }
         }
     }
-    
+
     public List<String> generatePalindromes(String s) {
         List<String> res = new ArrayList<>();
-        Map<Character, Integer> map = new HashMap<>();
-        int oddCount = 0;
+        if (s == null || s.length() == 0) return res;
         
-        for (int i = 0; i < s.length(); i++) {
-            char curr = s.charAt(i);
-            if (!map.containsKey(curr)) map.put(curr, 0);
-            map.put(curr, map.get(curr) + 1);
-            oddCount += map.get(curr) % 2 == 0 ? -1 : 1;
+        int len = s.length();
+        int oddCount = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for (char ch : s.toCharArray()) {
+            if (!map.containsKey(ch)) map.put(ch, 0);
+            map.put(ch, map.get(ch) + 1);
+            oddCount += map.get(ch) % 2 != 0 ? 1 : -1;
         }
         
-        /* not palindrome */
-        if (oddCount != 0 && oddCount != 1) return res;
+        if (oddCount != 1 && oddCount != 0) return res;
         
         String mid = "";
-        for (char ch : map.keySet()) {
-            if (map.get(ch) % 2 != 0) {
-                mid += ch;
-                map.put(ch, map.get(ch) - 1);
+        for (Map.Entry<Character, Integer> me : map.entrySet()) {
+            if (me.getValue() % 2 != 0) {
+                mid += me.getKey();
+                map.put(me.getKey(), me.getValue() - 1);
             }
         }
         
-        getAllPermutations(res, map, mid, s.length());
+        getAllPermutations(map, res, mid, len);
         
         return res;
     }
