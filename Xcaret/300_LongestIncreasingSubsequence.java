64cc613eb16f5069ca03ba189daf75cfa194f6e0
RuiLu <rlu0213@hotmail.com>
2016-11-08 17:36:08 -0500

    300

--- Xcaret/300_LongestIncreasingSubsequence.java
+++ Xcaret/300_LongestIncreasingSubsequence.java
@@ -1,52 +1,56 @@
 public class Solution {
     /**
      *  Idea -> Binary Search, sometimes you need to sacrifice space to speed up
+     *          We maintain a tmp array to store the longest increasing subsequence at every stage
+     *          If the current element is bigger the the biggest element in tmp, then add it to the tail
+     *          Else, we use binary search to find its position in tmp, then insert it into this position
      *  Time complexity -> O(nlogn)
      *  Space complexity -> O(n)
      */
-    private int findPosInTmp(int[] tmp, int lo, int hi, int target) {
+    private static int findPos(int lo, int hi, int[] tmp, int num) {
         while (lo <= hi) {
             int mid = (lo + hi) / 2;
-            if (tmp[mid] == target) return mid;
-            else if (tmp[mid] > target) hi = mid - 1;
+            if (tmp[mid] == num) return mid;
+            else if (tmp[mid] > num) hi = mid - 1;
             else lo = mid + 1;
         }
         return lo;
-    } 
-     
-    public int lengthOfLIS(int[] nums) {
+    }
+
+    public static int lengthOfLIS(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         
         int len = nums.length;
         int[] tmp = new int[len];
         int max = 0;
+        tmp[max] = nums[0];
         
-        tmp[max++] = nums[0]; 
-        
-        for (int i = 1; i < nums.length; i++) {
-            // we should use tmp[max-1] to compare, because it the last element inserted in tmp
-            if (nums[i] > tmp[max-1]) {
-                tmp[max++] = nums[i];
+        for (int i = 1; i < len; i++) {
+            if (nums[i] > tmp[max]) {
+                tmp[++max] = nums[i];
             } else {
-                int pos = findPosInTmp(tmp, 0, max - 1, nums[i]);
+                int pos = findPos(0, max, tmp, nums[i]);
                 tmp[pos] = nums[i];
             }
         }
         
-        return max;
+        return max + 1;
     }
     
     /**
-     *  Idea -> Dynamic Programming
+     *  Idea -> Dynamic Programming.
+     *          From 0 to len-1, check every element one by one.
+     *          For example, we work on nums[3] = 5, then we need to check elements from 0 to 2,
+     *          dp[i] = Math.max(dp[i], dp[j] + 1), where j >= 0 && j < i
      *  Time complexity -> O(n^2)
      *  Space complexity -> O(1)
      */
     public int lengthOfLIS(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         
+        int maxLen = Integer.MIN_VALUE;
         int len = nums.length;
         int[] dp = new int[len];
-        int res = 1;
         
         for (int i = 0; i < nums.length; i++) {
             dp[i] = 1;
@@ -57,9 +61,9 @@ public class Solution {
                 }
             }
             
-            res = Math.max(res, dp[i]);
+            maxLen = Math.max(maxLen, dp[i]);
         }
         
-        return res;
+        return maxLen;
     }
 }
