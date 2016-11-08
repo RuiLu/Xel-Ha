36a3d38e87279736f03aaf7dff7ac777cc630cd6
RuiLu <rlu0213@hotmail.com>
2016-11-08 12:03:11 -0500

    296

---
+++ Xcaret/296_BestMeetingPoint.java
@@ -0,0 +1,83 @@
+public class Solution {
+    /**
+     *  Idea -> As long as you have different numbers of people on your left and on your right, moving a little to the side
+     *          with more people to decrease the sum of distance
+     *          Solve it in one dimension first.
+     *  Reference -> https://discuss.leetcode.com/topic/27722/o-mn-java-2ms
+     *  Time complexity -> O(mn)
+     *  Space complexity -> O(mn)
+     */
+    public int minTotalDistance(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        
+        int minDistance = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        int[] helper = new int[row*col];
+        
+        for (int times = 0; times < 2; times++) {
+            int start = 0;
+            int count = 0;
+            
+            /* Row dimension first */
+            if (times == 0) {
+                for (int i = 0; i < row; i++) {
+                    for (int j = 0; j < col; j++) {
+                        if (grid[i][j] == 1) {
+                            helper[count++] = i;
+                        }
+                    }
+                }
+            } else {    /* Then col dimension */
+                for (int j = 0; j < col; j++) {
+                    for (int i = 0; i < row; i++) {
+                        if (grid[i][j] == 1) {
+                            helper[count++] = j;
+                        }
+                    }
+                }
+            }
+            
+            count--;
+            
+            while (start < count) {
+                minDistance += helper[count--] - helper[start++];    
+            }
+        }
+        
+        return minDistance;
+    } 
+     
+    /**
+     *  Idea -> Naive one, brute force
+     *  Time complexity -> O(mnp), where p is the number of people
+     */
+    public int minTotalDistance(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        
+        List<int[]> list = new ArrayList<>();
+        int row = grid.length;
+        int col = grid[0].length;
+        int minDistance = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    list.add(new int[]{i, j});
+                }
+            }
+        }
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                int total = 0;
+                for (int[] people : list) {
+                    total += Math.abs(people[0] - i) + Math.abs(people[1] - j);
+                }
+                minDistance = Math.min(minDistance, total);
+            }
+        }
+        
+        return minDistance == Integer.MAX_VALUE ? 1 : minDistance;
+    }
+}
