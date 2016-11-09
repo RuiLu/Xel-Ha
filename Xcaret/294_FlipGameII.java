772e845cb48e2e5164e012ade15e334ec12efe2d
RuiLu <rlu0213@hotmail.com>
2016-11-08 22:36:28 -0500

    294

--- Xcaret/294_FlipGameII.java
+++ Xcaret/294_FlipGameII.java
@@ -3,33 +3,28 @@ public class Solution {
      *  Idea -> backtracking
      *  Reference -> https://discuss.leetcode.com/topic/27250/share-my-java-backtracking-solution/9
      */
+    public static boolean canWin(String s) {
+        if (s == null || s.length() < 2) return false;
+        Map<String, Boolean> map = new HashMap<>();
+        return makeSureCanWin(s, map);
+    }
+    
     private static boolean makeSureCanWin(String s, Map<String, Boolean> map) {
         if (map.containsKey(s)) return map.get(s);
         
-        char prev = s.charAt(0);
-        for (int i = 1; i < s.length(); i++) {
-            char curr = s.charAt(i);
-            
-            if (prev == curr && curr == '+') {
-                String next = s.substring(0, i - 1) + "--" + s.substring(i + 1);
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
+                String next = s.substring(0, i) + "--" + s.substring(i + 2);
                 
-                /* Make sure that the opponent cannot win */
+                /* make sure the opponent cannot win. */
                 if (!makeSureCanWin(next, map)) {
                     map.put(s, true);
                     return true;
                 }
             }
-            
-            prev = curr;
         }
         
         map.put(s, false);
         return false;
-    }
-     
-    public static boolean canWin(String s) {
-        if (s == null || s.length() < 2) return false;
-        Map<String, Boolean> map = new HashMap<>();
-        return makeSureCanWin(s, map);
     }
 }
