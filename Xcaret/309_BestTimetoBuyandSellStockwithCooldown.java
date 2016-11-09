c3b52f0e50d38bdd8a439d3760d8ac9e57dd4bcd
RuiLu <rlu0213@hotmail.com>
2016-11-09 12:28:55 -0500

    309

--- Xcaret/309_BestTimetoBuyandSellStockwithCooldown.java
+++ Xcaret/309_BestTimetoBuyandSellStockwithCooldown.java
@@ -10,7 +10,7 @@ public class Solution {
      *  Space complexity -> O(n)
      */
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length < 2) return 0;
+        if (prices == null || prices.length <= 1) return 0;
         
         int len = prices.length;
         int[] s0 = new int[len];
