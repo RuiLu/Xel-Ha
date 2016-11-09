203c3b5db1108d6afc774b44845425eb9440444b
RuiLu <rlu0213@hotmail.com>
2016-11-09 12:11:27 -0500

    296

--- Xcaret/296_BestMeetingPoint.java
+++ Xcaret/296_BestMeetingPoint.java
@@ -10,74 +10,65 @@ public class Solution {
     public int minTotalDistance(int[][] grid) {
         if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
         
-        int minDistance = 0;
         int row = grid.length;
         int col = grid[0].length;
-        int[] helper = new int[row*col];
+        int[] tmp = new int[row*col];
+        int total = 0;
         
         for (int times = 0; times < 2; times++) {
             int start = 0;
             int count = 0;
-            
-            /* Row dimension first */
             if (times == 0) {
                 for (int i = 0; i < row; i++) {
                     for (int j = 0; j < col; j++) {
-                        if (grid[i][j] == 1) {
-                            helper[count++] = i;
-                        }
+                        if (grid[i][j] == 1) tmp[count++] = i;
                     }
                 }
-            } else {    /* Then col dimension */
+            } else if (times == 1) {
                 for (int j = 0; j < col; j++) {
                     for (int i = 0; i < row; i++) {
-                        if (grid[i][j] == 1) {
-                            helper[count++] = j;
-                        }
+                        if (grid[i][j] == 1) tmp[count++] = j;
                     }
                 }
             }
             
             count--;
-            
-            while (start < count) {
-                minDistance += helper[count--] - helper[start++];    
-            }
+            total += tmp[count--] - tmp[start++];
         }
         
-        return minDistance;
+        return total;
     } 
      
     /**
      *  Idea -> Naive one, brute force
      *  Time complexity -> O(mnp), where p is the number of people
      */
-    public int minTotalDistance(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+    // public int minTotalDistance(int[][] grid) {
+    //     if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
         
-        List<int[]> list = new ArrayList<>();
-        int row = grid.length;
-        int col = grid[0].length;
-        int minDistance = Integer.MAX_VALUE;
+    //     List<int[]> list = new ArrayList<>();
+    //     int row = grid.length;
+    //     int col = grid[0].length;
+    //     int minDistance = Integer.MAX_VALUE;
         
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == 1) {
-                    list.add(new int[]{i, j});
-                }
-            }
-        }
+    //     for (int i = 0; i < row; i++) {
+    //         for (int j = 0; j < col; j++) {
+    //             if (grid[i][j] == 1) {
+    //                 list.add(new int[]{i, j});
+    //             }
+    //         }
+    //     }
         
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                int total = 0;
-                for (int[] people : list) {
-                    total += Math.abs(people[0] - i) + Math.abs(people[1] - j);
-                }
-                minDistance = Math.min(minDistance, total);
-            }
-        }
+    //     for (int i = 0; i < row; i++) {
+    //         for (int j = 0; j < col; j++) {
+    //             int total = 0;
+    //             for (int[] people : list) {
+    //                 total += Math.abs(people[0] - i) + Math.abs(people[1] - j);
+    //             }
+    //             minDistance = Math.min(minDistance, total);
+    //         }
+    //     }
         
-        return minDistance == Integer.MAX_VALUE ? 1 : minDistance;
-    }
+    //     return minDistance == Integer.MAX_VALUE ? 1 : minDistance;
+    // }
 }
