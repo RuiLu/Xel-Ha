1b97c6e021289184771d4380ffc4b3bb4bbc8e00
RuiLu <rlu0213@hotmail.com>
2016-11-13 12:21:29 -0500

    295

--- Xcaret/295_FindMedianfromDataStream.java
+++ Xcaret/295_FindMedianfromDataStream.java
@@ -1,28 +1,27 @@
 public class MedianFinder {
-
-    private static PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-    private static PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-
+    /**
+     *  Idea -> https://discuss.leetcode.com/topic/27521/short-simple-java-c-python-o-log-n-o-1
+     *  Time complexity -> O(logn) for addNum()
+     *                     O(1) for findMedian
+     */
+    private PriorityQueue<Integer> large = new PriorityQueue<>();
+    private PriorityQueue<Integer> small = new PriorityQueue<>((a, b) -> b - a);
+    
     // Adds a number into the data structure.
     public void addNum(int num) {
-        maxHeap.offer(num);
-        minHeap.offer(maxHeap.poll());
-        if (minHeap.size() > maxHeap.size()) {
-            maxHeap.offer(minHeap.poll());
+        large.offer(num);
+        small.offer(large.poll());
+        if (large.size() < small.size()) {
+            large.offer(small.poll());
         }
     }
 
     // Returns the median of current data stream
     public double findMedian() {
-        if (minHeap.size() == 0 && maxHeap.size() == 0) return 0.0;
-        
-        if (minHeap.size() == maxHeap.size()) {
-            return (minHeap.peek() + maxHeap.peek()) / 2.0;
-        } else {
-            return maxHeap.peek() * 1.0;   
-        }
+        if (large.size() > small.size()) return large.peek() * 1.0;
+        else return (large.peek() + small.peek()) / 2.0;
     }
-};
+}
 
 // Your MedianFinder object will be instantiated and called as such:
 // MedianFinder mf = new MedianFinder();
