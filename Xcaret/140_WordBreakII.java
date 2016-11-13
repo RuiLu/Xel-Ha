198400d65d29434aaeb10d86b73d20d15cd7e3ab
RuiLu <rlu0213@hotmail.com>
2016-11-13 16:32:46 -0500

    140

--- Xcaret/140_WordBreakII.java
+++ Xcaret/140_WordBreakII.java
@@ -4,32 +4,31 @@ public class Solution {
      *  Time complexity -> O(len(wordDict) ^ len(s/minWordLen))
      */
     public List<String> wordBreak(String s, Set<String> wordDict) {
+        if (s == null || s.length() == 0 || wordDict.size() == 0) return new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         return helper(s, wordDict, map);
     }
     
     private List<String> helper(String s, Set<String> wordDict, Map<String, List<String>> map) {
-        List<String> list = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         
         if (s.equals("")) {
-            list.add("");
-            return list;
+            res.add("");
+            return res;
         }
         
-        if (map.containsKey(s)) {
-            return map.get(s);
-        }
+        if (map.containsKey(s)) return map.get(s);
         
         for (String word : wordDict) {
             if (s.startsWith(word)) {
                 List<String> next = helper(s.substring(word.length()), wordDict, map);
                 for (String str : next) {
-                    list.add(word + (str.equals("") ? "" : " " + str));
+                    res.add(word + (str.equals("") ? "" : " " + str));
                 }
             }
         }
         
-        map.put(s, list);
-        return list;
+        map.put(s, res);
+        return res;
     }
 }
