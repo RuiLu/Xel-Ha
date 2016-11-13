0be8fcf291aba5da9cda22f30f5ed288d49b5b91
RuiLu <rlu0213@hotmail.com>
2016-11-13 11:38:11 -0500

    232

--- Xcaret/232_ImplementQueueusingStacks.java
+++ Xcaret/232_ImplementQueueusingStacks.java
@@ -1,5 +1,33 @@
 class MyQueue {
     /**
+     *  Idea -> Using one Stack
+     */
+    private Stack<Integer> stack = new Stack<>();
+    
+    public void push(int x) {
+        Stack<Integer> tmp = new Stack<>();
+        while (!stack.isEmpty()) {
+            tmp.push(stack.pop());
+        }
+        stack.push(x);
+        while (!tmp.isEmpty()) {
+            stack.push(tmp.pop());
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int peek() {
+        return stack.peek();
+    }
+    
+    public boolean empty() {
+        return stack.isEmpty();
+    }
+    
+    /**
      *  Idea -> Using two stacks, inStack and outStack
      *          We update outStack only when pop() and peek() while outStack is empty, 
      *          and move elements from inStack to outStack
