2684a15f081ff4124590bc2db481af1b7be02826
RuiLu <rlu0213@hotmail.com>
2016-11-13 23:59:11 -0500

    299

--- Xcaret/299_BullsandCows.java
+++ Xcaret/299_BullsandCows.java
@@ -6,28 +6,29 @@ public class Solution {
      *  Space complexity -> O(n)
      */
     public String getHint(String secret, String guess) {
-        if (secret == null || guess == null || secret.length() != guess.length()) return null;
+        if (secret == null || guess == null || secret.length() != guess.length()) return "";
         
-        /* Since we have known all possible keys, we can use a array to work as Map */
-        int[] map = new int[10];
-        int matched = 0;
-        int unmatched = 0;
         char[] sca = secret.toCharArray();
         char[] gca = guess.toCharArray();
+        int matched = 0;
+        int unmatched = 0;
+        int[] map = new int[10];
         
+        /* First round, count matched number, and create map, counting elements that in secret while not in guess */
         for (int i = 0; i < sca.length; i++) {
             if (sca[i] == gca[i]) {
-                matched++; 
+                matched++;
             } else {
-                map[sca[i] - '0']++;
+                map[sca[i]-'0']++;
             }
         }
         
+        /* Second round, count unmathced number, counting element both in secret and guess but not matched */
         for (int i = 0; i < sca.length; i++) {
-            if (sca[i] == gca[i] || map[gca[i] - '0'] == 0) {
+            if (sca[i] == gca[i] || map[gca[i]-'0'] == 0) {
                 continue;
-            } else if (map[gca[i] - '0'] > 0) {
-                map[gca[i] -'0']--;
+            } else if (map[gca[i]-'0'] > 0) {
+                map[gca[i]-'0']--;
                 unmatched++;
             }
         }
