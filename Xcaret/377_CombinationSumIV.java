a568d83022cbc5f6719e06a9eaa2248311df4b71
RuiLu <rlu0213@hotmail.com>
2016-11-15 22:06:41 -0500

    377

---
+++ Xcaret/377_CombinationSumIV.java
@@ -0,0 +1,26 @@
+public class Solution {
+    /**
+     *  Idea -> Using DP
+     *  Time complexity -> O(mn), where m is target, n is the length of nums
+     */
+    public int combinationSum4(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return 0;
+        
+        Arrays.sort(nums);
+        int[] dp = new int[target+1];
+        
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num > i) {
+                    break;
+                } else if (num == i) {
+                    dp[i]++;
+                } else {
+                    dp[i] += dp[i-num];
+                }
+            }
+        }
+        
+        return dp[target];
+    }
+}
