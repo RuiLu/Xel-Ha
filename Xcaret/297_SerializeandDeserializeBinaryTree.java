ae617dd6243d929ff52afd5ee0e88b0be411c195
RuiLu <rlu0213@hotmail.com>
2016-11-16 23:15:20 -0500

    297

--- Xcaret/297_SerializeandDeserializeBinaryTree.java
+++ Xcaret/297_SerializeandDeserializeBinaryTree.java
@@ -12,45 +12,42 @@ public class Codec {
      *  Idea -> Serialize: using dfs to preorder traverse tree, when meeting null, using "n" to store.
      *          Deserialize: using a queue to store each token, and using dfs again to build the tree.
      */
+    
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        if (root == null) return sb.toString();
+        dfsSerialize(root, sb);
+        sb.setLength(sb.length() - 1);
+        return sb.toString();
+    }
+
     private void dfsSerialize(TreeNode node, StringBuilder sb) {
         if (node == null) {
             sb.append("n,");
             return;
         }
-        
-        sb.append(node.val + ",");
+        sb.append(node.val+",");
         dfsSerialize(node.left, sb);
         dfsSerialize(node.right, sb);
-    }
-
-    // Encodes a tree to a single string.
-    public String serialize(TreeNode root) {
-        StringBuilder sb = new StringBuilder();
-        dfsSerialize(root, sb);
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
-    }
-
-    private TreeNode dfsBuildTree(Deque<String> queue) {
-        String curr = queue.poll();
-        if (curr.equals("n")) return null;
-        
-        TreeNode root = new TreeNode(Integer.parseInt(curr));
-        root.left = dfsBuildTree(queue);
-        root.right = dfsBuildTree(queue);
-        
-        return root;
-    }
+    }    
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
         if (data == null || data.length() == 0) return null;
-        
         String[] tokens = data.split(",");
-        Deque<String> queue = new ArrayDeque<String>();
+        Queue<String> queue = new LinkedList<>();
         for (String token : tokens) queue.offer(token);
-        
-        return dfsBuildTree(queue);
+        return dfsDeserialize(queue);
+    }
+    
+    private TreeNode dfsDeserialize(Queue<String> queue) {
+        String token = queue.poll();
+        if (token.equals("n")) return null;
+        TreeNode node = new TreeNode(Integer.parseInt(token));
+        node.left = dfsDeserialize(queue);
+        node.right = dfsDeserialize(queue);
+        return node;
     }
 }
 
