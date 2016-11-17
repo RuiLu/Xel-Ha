493bd60acee2a51f759d80f90a13712423f3df1f
RuiLu <rlu0213@hotmail.com>
2016-11-16 23:00:38 -0500

    37

--- Xcaret/37_SudokuSolver.java
+++ Xcaret/37_SudokuSolver.java
@@ -4,18 +4,18 @@ public class Solution {
      */
     public void solveSudoku(char[][] board) {
         if (board == null || board.length == 0 || board[0].length == 0) return;
-        helper(board, 0, 0);
+        solver(board, 0, 0);
     }
     
-    private boolean helper(char[][] board, int i, int j) {
+    private boolean solver(char[][] board, int i, int j) {
         if (i == 9 && j == 0) return true;
         
-        if (board[i][j] != '.') return helper(board, j == 8 ? i + 1 : i, j == 8 ? 0 : j + 1);
+        if (board[i][j] != '.') return solver(board, j == 8 ? i + 1 : i, j == 8 ? 0 : j + 1);
         
         for (char ch = '1'; ch <= '9'; ch++) {
-            if (isValid(board, i, j, ch)) {
+            if (isValid(board, ch, i, j)) {
                 board[i][j] = ch;
-                if (helper(board, j == 8 ? i + 1 : i, j == 8 ? 0 : j + 1)) return true;
+                if (solver(board, j == 8 ? i + 1 : i, j == 8 ? 0 : j + 1)) return true;
                 board[i][j] = '.';
             }
         }
@@ -23,20 +23,20 @@ public class Solution {
         return false;
     }
     
-    private boolean isValid(char[][] board, int i, int j, char ch) {
+    private boolean isValid(char[][] board, char ch, int i, int j) {
         /* check row */
-        for (int k = 0; k < 9; k++) {
-            if (board[k][j] == ch) return false;
+        for (int row = 0; row < 9; row++) {
+            if (board[row][j] == ch) return false;
         }
         
         /* check col */
-        for (int k = 0; k < 9; k++) {
-            if (board[i][k] == ch) return false;
+        for (int col = 0; col < 9; col++) {
+            if (board[i][col] == ch) return false;
         }
         
         /* check box */
-        for (int x = i / 3 * 3; x < i / 3 * 3 + 3; x++) {
-            for (int y = j / 3 * 3 ; y < j / 3 * 3 + 3; y++) {
+        for (int x = (i/3*3); x < (i/3*3+3); x++) {
+            for (int y = (j/3*3); y < (j/3*3+3); y++) {
                 if (board[x][y] == ch) return false;
             }
         }
