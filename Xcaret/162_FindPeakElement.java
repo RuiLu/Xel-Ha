2da24e4ccbb948260e9ae1721766567091611ce0
RuiLu <rlu0213@hotmail.com>
2016-11-16 23:47:30 -0500

    162

--- Xcaret/162_FindPeakElement.java
+++ Xcaret/162_FindPeakElement.java
@@ -19,9 +19,9 @@ public class Solution {
                if (nums[mid] > nums[mid-1]) return mid;
                else return mid - 1;
            } else {
-               if (nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
-               else if (nums[mid] < nums[mid-1]) hi = mid - 1;
+               if (nums[mid] < nums[mid-1]) hi = mid - 1;
                else if (nums[mid] < nums[mid+1]) lo = mid + 1;
+               else return mid;
            }
        }
        
@@ -33,19 +33,19 @@ public class Solution {
      *  Time complexity -> O(n)
      *  Space complexity -> O(1)
      */
-    public int findPeakElement(int[] nums) {
-        if (nums == null || nums.length <= 1) return 0;
+    // public int findPeakElement(int[] nums) {
+    //     if (nums == null || nums.length <= 1) return 0;
         
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0) {
-                if (nums[i] > nums[i+1]) return i;
-            } else if (i == nums.length - 1) {
-                if (nums[i] > nums[i-1]) return i;
-            } else if (nums[i] > nums[i-1] && nums[i] > nums[i+1]) {
-                return i;
-            }
-        }
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (i == 0) {
+    //             if (nums[i] > nums[i+1]) return i;
+    //         } else if (i == nums.length - 1) {
+    //             if (nums[i] > nums[i-1]) return i;
+    //         } else if (nums[i] > nums[i-1] && nums[i] > nums[i+1]) {
+    //             return i;
+    //         }
+    //     }
         
-        return 0;
-    }
+    //     return 0;
+    // }
 }
