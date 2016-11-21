a26ddb68be8871fb5f596e273355e60713bb1341
RuiLu <rlu0213@hotmail.com>
2016-11-21 11:56:47 -0500

    220

--- Xcaret/220_ContainsDuplicateIII.java
+++ Xcaret/220_ContainsDuplicateIII.java
@@ -1,26 +1,53 @@
 public class Solution {
     /**
      *  Idea -> Binary Search Tree, using a TreeSet
-     *       -> TreeSet can act as a Binary Search Tree in this case
+     *          TreeSet(Red-Black tree) can act as a Binary Search Tree in this case
      *  Time complexity -> O(nlogk)
      */
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (k < 1 || t < 0 || nums == null || nums.length <= 1) return false;
-        
-        TreeSet<Integer> tset = new TreeSet<Integer>();
-        
+        if (k < 1 || t < 0 || nums == null || nums.length == 0) return false;
+        TreeSet<Long> tset = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
-            Integer floor = tset.ceiling(nums[i] - t);
-            Integer ceiling = tset.floor(nums[i] + t);
-            
-            if ((floor != null && nums[i] >= floor) || (ceiling != null && nums[i] <= ceiling)) {
+            Long floor = tset.ceiling((long)nums[i] - t);
+            Long ceiling = tset.floor((long)nums[i] + t);
+            if ((floor != null && floor <= (long)nums[i]) || (ceiling != null && ceiling >= (long)nums[i])) {
+                return true;
+            }
+            tset.add((long)nums[i]);
+            if (i - k >= 0) tset.remove((long)nums[i-k]);
+        }
+        return false;
+    }
+    
+    /**
+     *  Idea -> Bucket sort
+     *          For Java, it has a bit of problem because in java -3 / 5 = 0 unlike in python -3 / 5 = -1. We can use a              functiongetID to work around this.
+     *  Time complexity -> O(n)
+     */
+    private long getId(long i, long w) {
+        return i < 0 ? (i + 1) / w - 1 : i / w;
+    } 
+     
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (k < 1 || t < 0 || nums == null || nums.length == 0) return false;
+        
+        HashMap<Long, Long> map = new HashMap<>();
+        long w = (long)t + 1;
+     
+        for (int i = 0; i < nums.length; i++) {
+            long id = getId(nums[i], w); 
+            System.out.println(id);
+            if (map.containsKey(id)) {
+                return true;
+            } else if (map.containsKey(id - 1) && Math.abs(map.get(id - 1) - nums[i]) < w) {
+                return true;
+            } else if (map.containsKey(id + 1) && Math.abs(map.get(id + 1) - nums[i]) < w) {
                 return true;
             }
             
-            tset.add(nums[i]);
-            if (i >= k) tset.remove(nums[i-k]);
+            map.put(id, (long)nums[i]);
+            if (i >= k) map.remove(getId(nums[i-k], w));
         }
-        
         return false;
     }
 }
