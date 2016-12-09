adcfb34fa7a71aa32aa7ef08ee4e0667a5733dc4
RuiLu <rlu0213@hotmail.com>
2016-12-09 18:42:55 -0500

    380

--- Xcaret/380_InsertDeleteGetRandomInO1.java
+++ Xcaret/380_InsertDeleteGetRandomInO1.java
@@ -1,12 +1,15 @@
 public class RandomizedSet {
-    
-    private ArrayList<String> arr;
+    /**
+     *  Idea -> The key to this problem is that ArrayList's remove(Object obj) function takes O(1) time.
+     *          And there is not space complexity limitation
+     */
+    private ArrayList<String> list;
     private HashSet<Integer> set;
     private Random rand;
     
     /** Initialize your data structure here. */
     public RandomizedSet() {
-        arr = new ArrayList<String>();
+        list = new ArrayList<String>();
         set = new HashSet<Integer>();
         rand = new Random();
     }
@@ -14,21 +17,21 @@ public class RandomizedSet {
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
         if (!set.add(val)) return false;
-        arr.add(Integer.toString(val));
+        list.add(Integer.toString(val));
         return true;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
         if (!set.remove(val)) return false;
-        arr.remove(Integer.toString(val));
+        list.remove(Integer.toString(val));
         return true;
     }
     
     /** Get a random element from the set. */
     public int getRandom() {
-        int r = rand.nextInt(arr.size());
-        return Integer.parseInt(arr.get(r));
+        int r = rand.nextInt(list.size());
+        return Integer.parseInt(list.get(r));
     }
 }
 
