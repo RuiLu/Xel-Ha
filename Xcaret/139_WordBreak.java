5d16140d7985bb751f97038fe511bd36a6be56b3
RuiLu <rlu0213@hotmail.com>
2016-12-09 17:40:53 -0500

    139

--- Xcaret/139_WordBreak.java
+++ Xcaret/139_WordBreak.java
@@ -13,9 +13,9 @@ public class Solution {
         
         for (int i = 1; i <= len; i++) {
             for (int j = 0; j < i; j++) {
-                if (dp[j] && wordDict.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break;
+                if (wordDict.contains(s.substring(j, i))) {
+                    dp[i] = dp[j];
+                    if (dp[i]) break;
                 }
             }
         }
@@ -26,24 +26,24 @@ public class Solution {
     /**
      *  Idea -> DFS + memorization
      */
-    public boolean wordBreak(String s, Set<String> wordDict) {
-        if (s == null || s.length() == 0 || wordDict.size() == 0) return false;
-        Set<String> failed = new HashSet<>();
-        return helper(s, wordDict, failed);
-    }
-    
-    private boolean helper(String s, Set<String> wordDict, Set<String> failed) {
+    private boolean dfsHelper(String s, Set<String> wordDict, Set<String> failed) {
         if (s.equals("")) return true;
         if (failed.contains(s)) return false;
         
         for (int i = 1; i <= s.length(); i++) {
-            String next = s.substring(0, i);
-            if (wordDict.contains(next)) {
-                if (helper(s.substring(i), wordDict, failed)) return true;
+            String curr = s.substring(0, i);
+            if (wordDict.contains(curr)) {
+                if (dfsHelper(s.substring(i), wordDict, failed)) return true;
             }
         }
         
         failed.add(s);
         return false;
+    } 
+     
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        if (s == null || s.length() == 0 || wordDict.size() == 0) return false;
+        Set<String> failed = new HashSet<String>();
+        return dfsHelper(s, wordDict, failed);
     }
 }
