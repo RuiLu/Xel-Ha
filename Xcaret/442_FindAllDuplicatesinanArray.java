4ab3a702d57c17f2cc28a7eec8bfe55043dedc78
RuiLu <rlu0213@hotmail.com>
2016-12-12 14:01:25 -0500

    442

--- Xcaret/442_FindAllDuplicatesinanArray.java
+++ Xcaret/442_FindAllDuplicatesinanArray.java
@@ -7,7 +7,18 @@ public class Solution {
      *  Space complexity -> O(1)
      */
     public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<Integer>();
+        if (nums == null || nums.length <= 1) return list;
         
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                list.add(Math.abs(nums[i]));
+            }
+            nums[index] = -nums[index];
+        }
+        
+        return list;
     }
      
     /**
