bb5b3447a9e7c7e563607e07ea5d1a38bf94450f
RuiLu <rlu0213@hotmail.com>
2016-12-19 18:42:03 -0500

    215

--- ChichenItza/215_KthLargestElementinanArray.java
+++ ChichenItza/215_KthLargestElementinanArray.java
@@ -1,21 +1,21 @@
 public class Solution {
     /**
      *  Idea -> Use the same thought of quicksort, Divide and conquer.
-     *  Time complexity -> 
+     *  Time complexity -> O(n)
      *  Space complexity ->
      */
     public int findKthLargest(int[] nums, int k) {
-        if (nums == null || nums.length == 0) return -1;
+        if (nums == null || nums.length == 0 || nums.length < k) return 0;
         
         int lo = 0;
         int hi = nums.length - 1;
-        k = nums.length - k;  // because partition will more or less sort the array in increasing order
+        k = nums.length - k;
         
         while (lo <= hi) {
             int j = partition(nums, lo, hi);
             if (j == k) return nums[k];
-            else if (j < k) lo = j + 1;
-            else hi = j - 1;
+            else if (j > k) hi = j - 1;
+            else lo = j + 1;
         }
         
         return nums[k];
@@ -40,5 +40,22 @@ public class Solution {
         int tmp = nums[i];
         nums[i] = nums[j];
         nums[j] = tmp;
+    }
+    
+    /**
+     *  Idea -> Using MinHeap
+     *  Time complexity -> O(nlogk)
+     *  Space complexity -> O(k)
+     */
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || nums.length < k) return 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            pq.offer(nums[i]);
+            if (pq.size() > k) pq.poll();
+        }
+        
+        return pq.peek();
     }
 }
