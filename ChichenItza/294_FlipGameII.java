4ab9e97745d8468cebe7e2e5ea224f10e5013d7b
RuiLu <rlu0213@hotmail.com>
2016-12-30 10:58:58 -0500

    294

--- ChichenItza/294_FlipGameII.java
+++ ChichenItza/294_FlipGameII.java
@@ -4,23 +4,27 @@ public class Solution {
      *  Time complexity -> O(n!!), so much time
      */
     public boolean canWin(String s) {
-        if (s == null || s.length() <= 1) return false;
-        HashMap<String, Boolean> map = new HashMap<>();
-        return canWin(s, map);
+        if (s == null || s.length() < 2) return false;
+        HashMap<String, Boolean> memorization = new HashMap<>();
+        return canWin(s, memorization);
     }
     
-    private boolean canWin(String s, HashMap<String, Boolean> map) {
-        if (map.containsKey(s)) return map.get(s);
+    private boolean canWin(String s, HashMap<String, Boolean> memorization) {
+        if (memorization.containsKey(s)) return memorization.get(s);
+        
         for (int i = 0; i < s.length() - 1; i++) {
             if (s.substring(i, i+2).equals("++")) {
                 String next = s.substring(0, i) + "--" + s.substring(i+2);
-                if (!canWin(next, map)) {
-                    map.put(s, true);
+                /* if the opponent cannot win, then I can win */
+                if (!canWin(next, memorization)) {
+                    memorization.put(s, true);
                     return true;
                 }
             }
         }
-        map.put(s, false);
+        
+        /* If I cannot win, then the opponent can win. */
+        memorization.put(s, false);
         return false;
     }
 }
