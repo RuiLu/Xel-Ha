4d3e1a5c87be06d85d779e5ce5ed1a343ca84035
RuiLu <rlu0213@hotmail.com>
2017-01-05 21:16:20 -0500

    392

--- ChichenItza/392_IsSubsequence.java
+++ ChichenItza/392_IsSubsequence.java
@@ -4,16 +4,19 @@ public class Solution {
      *  Time complexity -> O(n)
      */
     public boolean isSubsequence(String s, String t) {
-        if (s == null || s.length() == 0) return true;
+        if (s == null || t == null) return false;
+        if (s.length() == 0) return true;
+        
+        /* Maintain two pointers, indexS and indexT, while traversing t */
         int indexS = 0;
         int indexT = 0;
         while (indexT < t.length()) {
-            if (s.charAt(indexS) == t.charAt(indexT)) {
-                indexS++;
-            }
+            /* Once the indexS == s.length(), finish finding, return true */
+            if (s.charAt(indexS) == t.charAt(indexT)) indexS++;
             if (indexS == s.length()) return true;
             indexT++;
         }
+        
         return false;
     }
     
@@ -23,27 +26,31 @@ public class Solution {
      *                     k is the average frequency of occurence of characters from s in t.
      */
     public boolean isSubsequence(String s, String t) {
-        if (s == null || s.length() == 0) return true;
+        if (s == null || t == null) return false;
+        if (s.length() == 0) return true;
         
+        /* Create an array for each character in t and its index(es). */
         List<Integer>[] lists = new List[256];
         
         for (int i = 0; i < t.length(); i++) {
             char ch = t.charAt(i);
-            if (lists[ch] == null) lists[ch] = new ArrayList<>();
+            if (lists[ch] == null) lists[ch] = new ArrayList<Integer>();
             lists[ch].add(i);
         }
         
+        /* Go through each character in s, starting with index 0 */
         int prev = 0;
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (lists[ch] == null) return false;
+            /* Use binary search to find index after prev. If not, return false; otherwise, assign index+1 to prev */
             int index = Collections.binarySearch(lists[ch], prev);
-            if (index < 0) index = -index - 1;
+            if (index < 0) index = -index-1;
+            /* Mean that search fails. */
             if (index == lists[ch].size()) return false;
-            prev = lists[ch].get(index) + 1;
+            prev = lists[ch].get(index)+1;
         }
         
         return true;
     }
-    
 }
