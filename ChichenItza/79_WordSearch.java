d98270a2cc70dc3704a4b153894c405ad4f71458
RuiLu <rlu0213@hotmail.com>
2017-01-10 10:36:46 -0500

    79

--- ChichenItza/79_WordSearch.java
+++ ChichenItza/79_WordSearch.java
@@ -1,32 +1,39 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
     /**
-     *  Idea -> Simple DFS, passing parent's value to children and compare their values.
-     *  Time complexity -> O(n)
-     *  Space complexity -> O(1)
-     */ 
-    private int maxLength = 0;
-    
-    public int longestConsecutive(TreeNode root) {
-        if (root == null) return maxLength;
-        helper(root, root.val-1, 0);
-        return maxLength;
+     *  Idea -> Backtracking
+     *  Time complexity -> O(mnl)
+     */
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || board[0].length == 0 || word == null || word.length() == 0) {
+            return false;
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (backtracking(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
     }
     
-    private void helper(TreeNode node, int target, int length) {
-        if (node == null) return;
-        if (node.val == target) length++;
-        else length = 1;
-        maxLength = Math.max(maxLength, length);
-        helper(node.left, node.val+1, length);
-        helper(node.right, node.val+1, length);
+    private boolean backtracking(char[][] board, String word, int i, int j, int idx) {
+        if (idx == word.length()) return true;
+        
+        char ch = word.charAt(idx);
+        
+        // If current position is beyond the given matrix, or this position has been visited,
+        // or the value of current position doesn't equal to the corresponding value on word.
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] == '#'|| ch != board[i][j] ) {
+            return false;
+        }
+        
+        // Do backtracking, '#' means this position has been visited.
+        board[i][j] = '#';
+        boolean res = backtracking(board, word, i-1, j, idx+1) ||
+                      backtracking(board, word, i, j-1, idx+1) ||
+                      backtracking(board, word, i+1, j, idx+1) ||
+                      backtracking(board, word, i, j+1, idx+1);
+        board[i][j] = ch;
+        
+        return res;
     }
 }
