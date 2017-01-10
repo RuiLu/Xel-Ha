825cd1944453f9aa049d05fb982f572bf13235ee
RuiLu <rlu0213@hotmail.com>
2017-01-10 10:39:19 -0500

    298

---
+++ ChichenItza/298_BinaryTreeLongestConsecutiveSequence.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     *  Idea -> Simple DFS, passing parent's value to children and compare their values.
+     *  Time complexity -> O(n)
+     *  Space complexity -> O(1)
+     */ 
+    private int maxLength = 0;
+    
+    public int longestConsecutive(TreeNode root) {
+        if (root == null) return maxLength;
+        helper(root, root.val, 0);
+        return maxLength;
+    }
+    
+    private void helper(TreeNode node, int target, int length) {
+        if (node == null) return;
+        if (node.val == target) length++;
+        else length = 1;
+        maxLength = Math.max(maxLength, length);
+        helper(node.left, node.val+1, length);
+        helper(node.right, node.val+1, length);
+    }
+}
