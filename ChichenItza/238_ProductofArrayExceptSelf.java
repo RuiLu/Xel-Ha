931da0210b8121bb9c4703170886dcc8779958e9
RuiLu <rlu0213@hotmail.com>
2017-01-11 09:47:28 -0500

    238

--- ChichenItza/238_ProductofArrayExceptSelf.java
+++ ChichenItza/238_ProductofArrayExceptSelf.java
@@ -17,7 +17,7 @@ public class Solution {
         
         int right = nums[len-1];
         for (int i = len-2; i >= 0; i--) {
-            res[i] = res[i]*right;
+            res[i] *= right;
             right *= nums[i];
         }
         
