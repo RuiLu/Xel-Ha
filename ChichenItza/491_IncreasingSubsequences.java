202dee97f6f39c3ca3fdc65341d059903b6c0fc1
RuiLu <rlu0213@hotmail.com>
2017-02-02 13:55:31 -0500

    491

--- ChichenItza/491_IncreasingSubsequences.java
+++ ChichenItza/491_IncreasingSubsequences.java
@@ -1,26 +1,24 @@
 public class Solution {
     /**
-     * Idea -> Use dfs and backtracking to calculate all position combinations
+     * Idea -> Use dfs and backtracking, and use a HashSet to deduplicate.
      */
     public List<List<Integer>> findSubsequences(int[] nums) {
-        /* Use Set to avoid dupslicate */
         Set<List<Integer>> res = new HashSet<>();
         if (nums == null || nums.length <= 1) return new ArrayList<>(res);
         List<Integer> tmp = new ArrayList<>();
-        dfs(res, tmp, nums, 0);
-        /* convert Set to List */
+        dfs(nums, res, tmp, 0);
         return new ArrayList<>(res);
     }
     
-    private void dfs(Set<List<Integer>> res, List<Integer> tmp, int[] nums, int idx) {
-        /* if subsequence's length is >= 2, add it into result */
+    private void dfs(int[] nums, Set<List<Integer>> res, List<Integer> tmp, int index) {
+        /* if size of tmp is equal to or bigger than 2, we should add it into res*/
         if (tmp.size() >= 2) res.add(new ArrayList<>(tmp));
         
-        for (int i = idx; i < nums.length; i++) {
-            /* only add element into tmp when tmp is empty or the last element of tmp is <= current element */
+        for (int i = index; i < nums.length; i++) {
+            /* if tmp is empty, or last element of tmp is equal to or smaller than the current element, add it into tmp */
             if (tmp.size() == 0 || tmp.get(tmp.size()-1) <= nums[i]) {
                 tmp.add(nums[i]);
-                dfs(res, tmp, nums, i+1);
+                dfs(nums, res, tmp, i+1);
                 tmp.remove(tmp.size()-1);
             }
         }
