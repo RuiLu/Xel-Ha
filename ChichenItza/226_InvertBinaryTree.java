c7379797acb176c2be89f12418d78e5d9d7ce530
RuiLu <rlu0213@hotmail.com>
2017-02-02 15:53:48 -0500

    226

---
+++ ChichenItza/226_InvertBinaryTree.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     * Idea -> DFS, switch the left and right pointers
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
