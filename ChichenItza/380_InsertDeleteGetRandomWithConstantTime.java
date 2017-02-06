5f500e9f17722ffeda020aec15aff3c6922040ed
RuiLu <rlu0213@hotmail.com>
2017-02-06 12:33:47 -0500

    380

--- ChichenItza/380_InsertDeleteGetRandomWithConstantTime.java
+++ ChichenItza/380_InsertDeleteGetRandomWithConstantTime.java
@@ -1,33 +1,87 @@
 public class RandomizedSet {
-
-    private static HashSet<Integer> set;
-    private static ArrayList<String> list;
-    private static Random rand;
-
+    /**
+     * Idea -> Use a HashMap, key is value, value is its position in ArrayList
+     *         When removing a value, if this value is the last one in ArrayList, do it directly;
+     *         Otherwise, swap this value with the value located on the last position of ArrayList.
+     *         Finally, remove the last value.
+     * 
+     * Follow-up -> Allow duplicate, we can modify Map<Integer, Integer> to Map<Integer, Set<Integer>>.
+     *              https://discuss.leetcode.com/topic/53216/java-solution-using-a-hashmap-and-an-arraylist-along-with-a-follow-up-131-ms/4
+     */
+    private Map<Integer, Integer> map;
+    private List<Integer> list;
+    private Random rand;
+    
     /** Initialize your data structure here. */
     public RandomizedSet() {
-        set = new HashSet<Integer>();
-        list = new ArrayList<String>();
+        map = new HashMap<>();
+        list = new ArrayList<>();
         rand = new Random();
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        if (!set.add(val)) return false;
-        return list.add(Integer.toString(val));
+        boolean contain = map.containsKey(val);
+        if (contain) return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
     }
-    
+
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        if (!set.remove(val)) return false;
-        return list.remove(Integer.toString(val));
+        boolean contain = map.containsKey(val);
+        if (!contain) return false;
+        
+        /* find the index of val */
+        int idx = map.get(val);
+        if (idx < list.size()-1) {
+            int lastVal = list.get(list.size()-1);
+            list.set(idx, lastVal);
+            map.put(lastVal, idx);
+        }
+        list.remove(list.size()-1);
+        map.remove(val);
+        
+        return true;
     }
-    
+
     /** Get a random element from the set. */
     public int getRandom() {
-        int r = rand.nextInt(list.size());
-        return Integer.parseInt(list.get(r));
+        return list.get(rand.nextInt(list.size()));
     }
+
+    /**
+     * Idea -> Convert Integer to String, because ArrayList.remove(String) takes O(1) time
+     */
+    // private static HashSet<Integer> set;
+    // private static ArrayList<String> list;
+    // private static Random rand;
+
+    // /** Initialize your data structure here. */
+    // public RandomizedSet() {
+    //     set = new HashSet<Integer>();
+    //     list = new ArrayList<String>();
+    //     rand = new Random();
+    // }
+    
+    // /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    // public boolean insert(int val) {
+    //     if (!set.add(val)) return false;
+    //     return list.add(Integer.toString(val));
+    // }
+    
+    // /** Removes a value from the set. Returns true if the set contained the specified element. */
+    // public boolean remove(int val) {
+    //     if (!set.remove(val)) return false;
+    //     return list.remove(Integer.toString(val));
+    // }
+    
+    // /** Get a random element from the set. */
+    // public int getRandom() {
+    //     int r = rand.nextInt(list.size());
+    //     return Integer.parseInt(list.get(r));
+    // }
 }
 
 /**
