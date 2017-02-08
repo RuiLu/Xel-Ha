4287fac99962551da99637053ea60180f259f598
RuiLu <rlu0213@hotmail.com>
2017-02-08 09:06:19 -0500

    450

--- ChichenItza/450_DeleteNodeinaBST.java
+++ ChichenItza/450_DeleteNodeinaBST.java
@@ -18,27 +18,27 @@ public class Solution {
      *              then assign that value to the current found node, then recursively delete the min node
      */
     public TreeNode deleteNode(TreeNode root, int key) {
-        if (root == null) return null;
-        
-        if (root.val > key) {
-            root.left = deleteNode(root.left, key);
-        } else if (root.val < key) {
-            root.right = deleteNode(root.right, key);
-        } else {
-            /* 1. has no subtree */
-            if (root.left == null && root.right == null) return null;
-            
-            /* 2. has only one non-null subtree */
-            if (root.left == null) return root.right;
-            else if (root.right == null) return root.left;
-            
-            /* 3. has both subtrees */
-            TreeNode min = findMinNode(root.right);
-            root.val = min.val;
-            root.right = deleteNode(root.right, min.val);
+       if (root == null) return null;
+       
+       if (key < root.val) {
+           root.left = deleteNode(root.left, key);
+       } else if (key > root.val) {
+           root.right = deleteNode(root.right, key);
+       } else {
+           /* case 1 -> both subtrees are null */
+           if (root.left == null && root.right == null) return null;
+           
+           /* case 2 -> One of the subtree is null */
+           if (root.left == null) return root.right;
+           else if (root.right == null) return root.left;
+           
+           /* case 3 -> both subtree are not null */
+           TreeNode minNode = findMinNode(root.right);
+           root.val = minNode.val;
+           root.right = deleteNode(root.right, minNode.val);
         }
-        
-        return root;
+       
+        return root;   
     }
     
     private TreeNode findMinNode(TreeNode node) {
