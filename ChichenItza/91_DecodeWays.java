36dd4c43e645ee9a5c232839e01d5fccd5a09385
RuiLu <rlu0213@hotmail.com>
2017-02-10 12:14:12 -0500

    91

---
+++ ChichenItza/91_DecodeWays.java
@@ -0,0 +1,32 @@
+public class Solution {
+    /**
+     * Idea -> Dynamic programming 
+     */
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) return 0;
+        
+        int len = s.length();
+        int[] dp = new int[len+1];
+        
+        dp[0] = 1;
+        
+        /* check the first character of string */
+        if (isValid(s.substring(0, 1))) dp[1] = 1;
+        else dp[1] = 0;
+        
+        for (int i = 2; i <= len; i++) {
+            if (isValid(s.substring(i-1, i))) dp[i] += dp[i-1];
+            if (isValid(s.substring(i-2, i))) dp[i] += dp[i-2];
+        }
+        
+        return dp[len];
+    }
+    
+    private boolean isValid(String s) {
+        if (s == null || s.length() == 0) return false;
+        /* if the first character of s is 0, which means invalid */
+        if (s.charAt(0) == '0') return false;
+        int value = Integer.parseInt(s);
+        return value >= 1 && value <= 26;
+    }
+}
